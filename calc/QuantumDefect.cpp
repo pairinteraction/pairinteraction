ca2ace6201adb156f336e489895f955cb44b2176
Henri Menke <h.menke@fkf.mpg.de>
2016-11-30 19:01:38 +0100

    SQLite refactor: make db handling header only

--- calc/QuantumDefect.cpp
+++ calc/QuantumDefect.cpp
@@ -40,7 +40,7 @@ QuantumDefect::QuantumDefect(std::string species, int n, int l, real_t j)
     rc(rc_), nstar(nstar_), energy(energy_)
 {
   std::stringstream ss;
-  SQLite3 db("databases/quantum_defects.db", SQLITE_OPEN_READONLY);
+  sqlite::handle db("databases/quantum_defects.db", SQLITE_OPEN_READONLY);
   int pot_max_l, ryd_max_l;
   int pot_l, ryd_l;
   real_t ryd_max_j;
@@ -49,7 +49,7 @@ QuantumDefect::QuantumDefect(std::string species, int n, int l, real_t j)
   // Determine maximal L for model potentials
   ss.str(std::string());
   ss << "select MAX(L) from model_potential where (element = '" << species << "');";
-  SQLite3Result res1 = db.query(ss.str().c_str());
+  sqlite::result res1 = db.query(ss.str().c_str());
   ss.str(std::string());
   if (res1.size() > 0)
     *res1.first() >> pot_max_l;
@@ -61,7 +61,7 @@ QuantumDefect::QuantumDefect(std::string species, int n, int l, real_t j)
   // Determine maximal L for Rydberg-Ritz coefficients
   ss.str(std::string());
   ss << "select MAX(L) from rydberg_ritz where (element = '" << species << "');";
-  SQLite3Result res2 = db.query(ss.str().c_str());
+  sqlite::result res2 = db.query(ss.str().c_str());
   ss.str(std::string());
   if (res2.size() > 0)
     *res2.first() >> ryd_max_l;
@@ -76,7 +76,7 @@ QuantumDefect::QuantumDefect(std::string species, int n, int l, real_t j)
      << "(element = '" << species << "') "
      << "and (L = " << ryd_l << ") "
      << ");";
-  SQLite3Result res3 = db.query(ss.str().c_str());
+  sqlite::result res3 = db.query(ss.str().c_str());
   ss.str(std::string());
   if (res3.size() > 0)
     *res3.first() >> ryd_max_j;
@@ -91,7 +91,7 @@ QuantumDefect::QuantumDefect(std::string species, int n, int l, real_t j)
      << "(element = '" << species << "') "
      << "and (L = " << pot_l << ") "
      << ");";
-  SQLite3Result res4 = db.query(ss.str().c_str());
+  sqlite::result res4 = db.query(ss.str().c_str());
   ss.str(std::string());
   if (res4.size() > 0)
     *res4.first() >> ac_ >> Z_ >> a1_ >> a2_ >> a3_ >> a4_ >> rc_;
@@ -105,7 +105,7 @@ QuantumDefect::QuantumDefect(std::string species, int n, int l, real_t j)
      << "and (L = " << ryd_l << ") "
      << "and (J = " << ryd_j << ") "
      << ");";
-  SQLite3Result res = db.query(ss.str().c_str());
+  sqlite::result res = db.query(ss.str().c_str());
   ss.str(std::string());
   nstar_ = n;
   real_t Ry_inf = 109737.31568525; // TODO kann man hier wirklich immer den selben Wert verwenden?
--- calc/SQLite.h
+++ calc/SQLite.h
@@ -25,42 +25,198 @@
 #include <memory>
 #include "Iter.h"
 
-class SQLite3Result {
+
+namespace sqlite {
+
+struct sqlite_error : public std::exception {
+public:
+    explicit sqlite_error(std::string msg)
+        : m_msg(std::string("SQLite error: ") + msg)
+    {}
+
+    const char* what() const noexcept
+    {
+        return m_msg.c_str();
+    }
+
+private:
+    std::string m_msg;
+};
+
+
+class result {
 public:
     class iterator {
     public:
-        iterator(const SQLite3Result* result, int pos, int nColumn);
-        bool operator!= (const iterator& other) const;
-        std::unique_ptr<std::stringstream> operator* () const;
-        const iterator& operator++ ();
+        explicit iterator(const result* res, int pos, int nColumn)
+            : res(res), pos(pos), nColumn(nColumn)
+        {}
+
+        bool operator!= (const iterator& other) const
+        {
+            return pos != other.pos;
+        }
+
+        std::unique_ptr<std::stringstream> operator* () const
+        {
+            return res->getRow(pos);
+        }
+
+        const iterator& operator++ ()
+        {
+            ++pos;
+            return *this;
+        }
+
     private:
-        const SQLite3Result* result;
+        const result* res;
         int pos, nColumn;
         int rc;
     };
-    SQLite3Result();
-    ~SQLite3Result();
-    size_t size() const;
-    SQLite3Result::iterator begin() const;
-    SQLite3Result::iterator end() const;
-    std::unique_ptr<std::stringstream> first() const;
-    std::unique_ptr<std::stringstream> header() const;
-    std::unique_ptr<std::stringstream> getRow(int pos) const;
+
+    explicit result()
+        : azResult(NULL), nRow(0), nColumn(0)
+    {}
+
+    result( result& r )
+    {
+        swap( r );
+        // invalidate copied result to prevent deletion
+        r.azResult = nullptr;
+    }
+
+    result( result&& r )
+    {
+        swap( r );
+        // invalidate copied result to prevent deletion
+        r.azResult = nullptr;
+    }
+
+    void swap( result& r )
+    {
+        std::swap ( nRow    , r.nRow     );
+        std::swap ( nColumn , r.nColumn  );
+        std::swap ( azResult, r.azResult );
+    }
+
+    void swap( result&& r )
+    {
+        std::swap ( nRow    , r.nRow     );
+        std::swap ( nColumn , r.nColumn  );
+        std::swap ( azResult, r.azResult );
+    }
+
+    ~result()
+    {
+        sqlite3_free_table(azResult);
+    }
+
+    size_t size() const
+    {
+        return nRow;
+    }
+
+    result::iterator begin() const
+    {
+        return result::iterator(this, 0, nColumn);
+    }
+
+    result::iterator end() const
+    {
+        return result::iterator(this, nRow, nColumn);
+    }
+
+    std::unique_ptr<std::stringstream> first() const
+    {
+        return getRow(0);
+    }
+
+    std::unique_ptr<std::stringstream> header() const
+    {
+        return getRow(-1);
+    }
+
+    std::unique_ptr<std::stringstream> getRow(int pos) const
+    {
+        std::unique_ptr<std::stringstream> ss( new std::stringstream() );
+        std::string spacer = "";
+        for (int i = 0; i < nColumn; ++i) {
+            *ss << spacer << azResult[(pos+1)*nColumn+i];
+            spacer = " ";
+        }
+        return ss;
+    }
+
     char **azResult;
     int nRow, nColumn;
+
+private:
+    result& operator=( result const& );
+    result& operator=( result&& );
 };
 
-class SQLite3 {
+class handle {
 public:
-    SQLite3(const std::string filename);
-    SQLite3(const std::string filename, int flags);
-    ~SQLite3();
-    SQLite3Result query(const std::string sql);
-    void exec(const std::string sql);
+    explicit handle(const std::string filename)
+        : zErrMsg(NULL)
+    {
+        if ( sqlite3_open(filename.c_str(), &db) )
+        {
+            sqlite3_close(db);
+            throw sqlite_error( sqlite3_errmsg(db) );
+        }
+    }
+
+    explicit handle(const std::string filename, int flags)
+        : zErrMsg(NULL)
+    {
+        if ( sqlite3_open_v2(filename.c_str(), &db, flags, NULL) )
+        {
+            sqlite3_close(db);
+            throw sqlite_error( sqlite3_errmsg(db) );
+        }
+    }
+
+    ~handle()
+    {
+        sqlite3_close(db);
+    }
+
+    result query(const std::string sql)
+    {
+        result res;
+        if ( sqlite3_get_table(db, sql.c_str(), &res.azResult, &res.nRow, &res.nColumn, &zErrMsg) != SQLITE_OK ) {
+            std::string msg("SQL error: ");
+            msg += zErrMsg;
+            sqlite3_free(zErrMsg);
+            throw sqlite_error(msg);
+        }
+        return res;
+    }
+
+    void exec(const std::string sql)
+    {
+        if ( sqlite3_exec(db, sql.c_str(), NULL, NULL, &zErrMsg) != SQLITE_OK ) {
+            std::string msg("SQL error: ");
+            msg += zErrMsg;
+            sqlite3_free(zErrMsg);
+            throw sqlite_error(msg);
+        }
+    }
+
 private:
+    handle( handle const& );
+    handle& operator=( handle const& );
+
+    handle( handle&& );
+    handle& operator=( handle&& );
+
     sqlite3 *db;
     char *zErrMsg;
     int rc;
 };
+
+} // namespace sqlite
+
 
 #endif // SQLITE_BINDINGS
