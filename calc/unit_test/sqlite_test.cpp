caa4fc973579e430874e1840169bfc72b91845ae
Henri Menke <phy86901@stud.uni-stuttgart.de>
2017-02-07 20:11:54 +0100

    Extended sqlite test case

--- calc/unit_test/sqlite_test.cpp
+++ calc/unit_test/sqlite_test.cpp
@@ -14,6 +14,9 @@
  * limitations under the License.
  */
 
+#include <string>
+#include <sstream>
+#include <vector>
 #include "SQLite.h"
 #define BOOST_TEST_MODULE SQLite interface test
 #include <boost/test/unit_test.hpp>
@@ -22,6 +25,35 @@ BOOST_AUTO_TEST_CASE( sqlite_query_test )
 {
   // Open an in-memory database for tests
   sqlite::handle db(":memory:");
-  
-  BOOST_CHECK_THROW( db.query("This is not valid SQL"), sqlite::sqlite_error );
+
+  BOOST_CHECK_THROW( db.query("This is not valid SQL"),
+                     sqlite::sqlite_error );
+  BOOST_CHECK_THROW( db.exec("Neither is this"),
+                     sqlite::sqlite_error );
+
+  // Check string calling
+  std::string string_query( "create table test(text);" );
+  BOOST_CHECK_EQUAL( db.query( string_query ).size(), 0 );
+
+  // Check stringstream calling
+  std::stringstream ss_query;
+  ss_query << "insert into test values(\"Hello World!\");";
+  ss_query << "insert into test values(\"Goodbye Earth!\");";
+  BOOST_CHECK_EQUAL( db.query( ss_query ).size(), 0 );
+
+  // Check result
+  sqlite::result res = db.query("select * from test;");
+  BOOST_CHECK_EQUAL( res.size(), 2 );
+  std::string entry = *res.begin();
+  BOOST_CHECK_EQUAL( entry, "Hello World!" );
+
+  // Check if result is iterable
+  std::vector < std::string > values;
+  values.reserve( res.size() );
+  for ( auto const& row : res )
+  {
+    values.push_back( std::string(row) );
+  }
+  BOOST_CHECK_EQUAL( values[0], "Hello World!"   );
+  BOOST_CHECK_EQUAL( values[1], "Goodbye Earth!" );
 }
