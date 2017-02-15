8cd1949973cb4c403f64ce27035f11bd66bd112b
Henri Menke <phy86901@stud.uni-stuttgart.de>
2017-02-15 13:29:37 +0100

    [doxygen] Auto-update gh-pages

---
+++ doc/doxygen/publish_doxygen.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+set -e
+
+
+function gh_pages_prepare()
+{
+    mkdir -p "${TRAVIS_BUILD_DIR}/build/doc/doxygen/";
+    cd "${TRAVIS_BUILD_DIR}/build/doc/doxygen/";
+
+    git clone -b gh-pages https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} html;
+
+    cd "html";
+    
+    git config user.name "Travis CI";
+    git config user.email "travis@travis-ci.org";
+
+    rm -f .git/index;
+    git clean -df;
+}
+
+
+function gh_pages_generate()
+{
+    cd "${TRAVIS_BUILD_DIR}/build";
+
+    cmake -DWITH_BACKEND=No -DWITH_GUI=No -DWITH_DATABASE=No ..
+
+    make doc
+}
+
+
+function gh_pages_update()
+{
+    cd "${TRAVIS_BUILD_DIR}/build/doc/doxygen/html";
+
+    touch .nojekyll;
+
+    git add --all;
+
+    git commit -m "Doxygen build from Travis build ${TRAVIS_BUILD_NUMBER} for commit ${TRAVIS_COMMIT}"
+
+    git push -f origin gh-pages
+}
+
+
+cd "${TRAVIS_BUILD_DIR}";
+gh_pages_prepare;
+gh_pages_generate;
+gh_pages_update;
