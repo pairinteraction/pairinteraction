08d14b4420e8579973175e4410fbac8265dbbeb7
Sebastian Weber <seweber@posteo.de>
2017-03-31 00:50:10 +0200

    Check for OAuth token instead of main repository

--- doc/doxygen/publish_doxygen.sh
+++ doc/doxygen/publish_doxygen.sh
@@ -19,8 +19,8 @@ if [ "${TRAVIS_BRANCH}" != "master" ]; then
 fi;
 
 
-if [ "${TRAVIS_REPO_SLUG}" != "pairinteraction/pairinteraction" ]; then
-    echo "INFO: We are not on the main repository.";
+if [ -z "$GH_TOKEN" ]; then
+    echo "INFO: The GitHub access token is not set.";
     echo "INFO: Not building docs.";
     exit 0;
 fi;
