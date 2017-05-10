d366c441e65ffef355988e40e6f434eb6fefba10
Henri Menke <henri@icp.uni-stuttgart.de>
2017-05-10 16:39:43 +1200

    [swig] Wrap dtypes to have symmetries in Python

--- calc/dtypes.h
+++ calc/dtypes.h
@@ -22,8 +22,6 @@
 #include <Eigen/Sparse>
 #include <Eigen/Dense>
 
-constexpr auto ORDER = Eigen::ColMajor;
-
 typedef double real_t; // TODO we should always use double otherwise the diagonalization results are very noisy
 typedef std::complex<real_t> complex_t;
 typedef uint32_t idx_t;
@@ -43,13 +41,13 @@ typedef real_t scalar_t;
 
 typedef Eigen::Triplet<scalar_t> eigen_triplet_t;
 typedef Eigen::Triplet<real_t> eigen_triplet_real_t;
-typedef Eigen::SparseMatrix<scalar_t, ORDER, eigen_idx_t> eigen_sparse_t;
-typedef Eigen::SparseMatrix<real_t, ORDER, eigen_idx_t> eigen_sparse_real_t;
-typedef Eigen::SparseMatrix<scalar_t, ORDER, eigen_idx_t>::InnerIterator eigen_iterator_t;
-typedef Eigen::SparseMatrix<real_t, ORDER, eigen_idx_t>::InnerIterator eigen_iterator_real_t;
-typedef Eigen::Matrix<scalar_t,Eigen::Dynamic,Eigen::Dynamic,ORDER> eigen_dense_t;
-typedef Eigen::Matrix<scalar_t,Eigen::Dynamic,1,ORDER> eigen_vector_t;
-typedef Eigen::Matrix<real_t,Eigen::Dynamic,1,ORDER> eigen_vector_real_t;
+typedef Eigen::SparseMatrix<scalar_t, Eigen::ColMajor, eigen_idx_t> eigen_sparse_t;
+typedef Eigen::SparseMatrix<real_t, Eigen::ColMajor, eigen_idx_t> eigen_sparse_real_t;
+typedef Eigen::SparseMatrix<scalar_t, Eigen::ColMajor, eigen_idx_t>::InnerIterator eigen_iterator_t;
+typedef Eigen::SparseMatrix<real_t, Eigen::ColMajor, eigen_idx_t>::InnerIterator eigen_iterator_real_t;
+typedef Eigen::Matrix<scalar_t,Eigen::Dynamic,Eigen::Dynamic,Eigen::ColMajor> eigen_dense_t;
+typedef Eigen::Matrix<scalar_t,Eigen::Dynamic,1,Eigen::ColMajor> eigen_vector_t;
+typedef Eigen::Matrix<real_t,Eigen::Dynamic,1,Eigen::ColMajor> eigen_vector_real_t;
 
 
 class Triple {
