dea610d8b03c8ad585f59d65c4bd8a531b45d1c8
Henri Menke <henri@icp.uni-stuttgart.de>
2017-09-20 16:31:07 +1200

    [tests] Increase communication timeout to 1 sec for Mac OS X; clang-format

--- calc/unit_test/communication_test.cpp
+++ calc/unit_test/communication_test.cpp
@@ -21,44 +21,48 @@
 #define BOOST_TEST_MODULE ZeroMQ interface test
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE( exceptions_test )
+BOOST_AUTO_TEST_CASE(exceptions_test)
 {
-  auto context = zmq::context();
-  auto socket  = context.socket(ZMQ_REQ);
-  
-  BOOST_CHECK_THROW( context.socket ( -42 )       , zmq::error );
-  BOOST_CHECK_THROW( socket .bind   ( "garbage" ) , zmq::error );
-  BOOST_CHECK_THROW( socket .connect( "garbage" ) , zmq::error );
+    auto context = zmq::context();
+    auto socket = context.socket(ZMQ_REQ);
 
-  try { context.socket ( -42 ); }
-  catch(zmq::error const& e) { e.what(); }
+    BOOST_CHECK_THROW(context.socket(-42), zmq::error);
+    BOOST_CHECK_THROW(socket.bind("garbage"), zmq::error);
+    BOOST_CHECK_THROW(socket.connect("garbage"), zmq::error);
+
+    try {
+        context.socket(-42);
+    } catch (zmq::error const &e) {
+        e.what();
+    }
 }
 
-BOOST_AUTO_TEST_CASE( send_test )
+BOOST_AUTO_TEST_CASE(send_test)
 {
-  auto context = zmq::context();
-  constexpr static char msg[] = "Into the void...";
-  constexpr static size_t len = sizeof(msg);
+    auto context = zmq::context();
+    constexpr static char msg[] = "Into the void...";
+    constexpr static size_t len = sizeof(msg);
 
-  std::thread sender([&context]() {
-      auto publisher = context.socket(ZMQ_PUB);
-      publisher.bind("tcp://*:5555");
+    std::thread sender([&context]() {
+        auto publisher = context.socket(ZMQ_PUB);
+        publisher.bind("tcp://*:5555");
 
-      std::chrono::milliseconds ms{100};
-      std::this_thread::sleep_for(ms); // wait for the others to receive
-      BOOST_CHECK_EQUAL( publisher.send(msg), len-1 );
+        std::this_thread::sleep_for(
+            std::chrono::seconds{1}); // wait for the others to receive
+        BOOST_CHECK_EQUAL(publisher.send(msg), len - 1);
     });
 
-  std::thread receiver([&context]() {
-      auto subscriber = context.socket(ZMQ_SUB);
-      subscriber.connect("tcp://localhost:5555");
-      BOOST_CHECK_EQUAL( zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, nullptr, 0), 0);
+    std::thread receiver([&context]() {
+        auto subscriber = context.socket(ZMQ_SUB);
+        subscriber.connect("tcp://localhost:5555");
+        BOOST_CHECK_EQUAL(zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, nullptr, 0),
+                          0);
 
-      char buf[len];
-      BOOST_CHECK_EQUAL( zmq_recv(subscriber, buf, len, 0), len );
-      BOOST_CHECK_EQUAL( msg, buf );
+        char buf[len];
+        BOOST_CHECK_EQUAL(zmq_recv(subscriber, buf, len, 0), len);
+        BOOST_CHECK_EQUAL(msg, buf);
     });
 
-  sender.join();
-  receiver.join();
+    sender.join();
+    receiver.join();
 }
