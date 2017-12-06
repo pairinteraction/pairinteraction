ecada355cf26f309dbfad595ef7f1709bd8dc351
Sebastian Weber <seweber@users.noreply.github.com>
2017-12-06 10:09:37 +0100

    Fix tutorial for Windows, improve class documentation (#73)
    * Fix tutorial for Windows
    
    * [sphinx] More useful class documentation

--- doc/sphinx/examples/comparison_to_saffman_fig13.py
+++ doc/sphinx/examples/comparison_to_saffman_fig13.py
@@ -2,12 +2,13 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 from multiprocessing import Pool
+from functools import partial
 import sys
 if sys.platform == "darwin": sys.path.append("/Applications/pairinteraction.app/Contents/Resources")
 elif sys.platform == "win32": sys.path.append("C:\Program Files\pairinteraction")
 from libpairinteraction import pireal as pi
 
-distance = 10 # µm
+distance = 10 # um
 bfields = np.linspace(0, 20, 200) # Gauss
 
 state_one = pi.StateOne("Rb", 43, 2, 2.5, 0.5)
@@ -24,7 +25,7 @@ def setup_system_one(bfield):
     system_one.setBfield([0, 0, bfield])
     return system_one
 
-def setup_system_two(system_one,angle):
+def setup_system_two(system_one, angle):
     system_two = pi.SystemTwo(system_one, system_one, "./cache")
     system_two.restrictEnergy(state_two.energy-5, state_two.energy+5)
     system_two.setDistance(10)
@@ -34,13 +35,13 @@ def setup_system_two(system_one,angle):
     system_two.setConservedParityUnderPermutation(pi.ODD)
     return system_two
 
-def getEnergies(bfield):
+def getEnergies(bfield, angle):
     # Set up one atom system
     system_one = setup_system_one(bfield)
     system_one.diagonalize()
 
     # Calculate Zeeman shift
-    zeemanshift = 2*system_one.diagonal[system_one.getVectorindex(state_one)]
+    zeemanshift = 2*system_one.diagonal[system_one.getVectorindex(state_one)] # GHz
 
     # Set up two atom system
     system_two = setup_system_two(system_one,angle)
@@ -59,13 +60,10 @@ if __name__ ==  '__main__':
     plt.xlim(-0.4,20.4)
     plt.ylim(0,0.4)
 
-    angle = 0 # rad
     with Pool() as pool:
-        energies1 = pool.map(getEnergies, bfields)
-
-    angle = np.pi/2 # rad
+        energies1 = pool.map(partial(getEnergies, angle=0), bfields)
     with Pool() as pool:
-        energies2 = pool.map(getEnergies, bfields)
+        energies2 = pool.map(partial(getEnergies, angle=np.pi/2), bfields)
 
     plt.plot(bfields, energies1, 'b-', label=r"$\theta = 0$")
     plt.plot(bfields, energies2, 'g-', label=r"$\theta = \pi/2$")
