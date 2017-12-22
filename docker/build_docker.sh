3def2e829dd82b5c78ab0a73fa21a7c5a424b426
Henri Menke <henri@icp.uni-stuttgart.de>
2017-12-22 17:37:51 +1300

    [docker] Switch build order
    This reordering might fix the incomplete code coverage reports on
    Codecov.  The problem is, that publish_documentation.sh cleans the build
    directory of untracked files and therefore might remove coverage
    reports.  If this still does not work we need a separate documentation
    build again.

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -33,7 +33,7 @@ case "${TRAVIS_OS_NAME}" in
                            -v ${TRAVIS_BUILD_DIR}:${SOURCE_DIR} \
                            --interactive --tty \
                            pairinteraction/$image \
-                           /bin/bash -c "/bin/bash /travis/docker/build_cmake.sh gcov; /bin/bash /travis/doc/publish_documentation.sh"
+                           /bin/bash -c "/bin/bash /travis/doc/publish_documentation.sh; /bin/bash /travis/docker/build_cmake.sh gcov"
                     ;;
 
                 *)
