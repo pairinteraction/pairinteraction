fe44b202750e2063fc854a72ea66bd93ada5fca4
Sebastian Weber <seweber@posteo.de>
2018-05-28 16:51:08 +0200

    [tests] Extend tests of StateOne and StateTwo

--- testsuite/state.py
+++ testsuite/state.py
@@ -6,43 +6,49 @@ au2GHz = 6579683.920711
 class TestState(unittest.TestCase):
 
     def test_instantiation(self):
-        s = pi.StateOne("Rb", 79, 1, 1.5, 0.5)
+        s = pi.StateOne("Sr3", 79, 1, 2, 0)
         self.assertEqual(type(s), pi.StateOne)
 
     def test_properties(self):
-        s = pi.StateOne("Rb", 79, 1, 1.5, 0.5)
-        self.assertEqual(s.element, "Rb")
+        s = pi.StateOne("Sr3", 79, 1, 2, 0)
+        self.assertEqual(s.species, "Sr3")
+        self.assertEqual(s.element, "Sr")
+        self.assertEqual(s.s, 1)
         self.assertEqual(s.n, 79)
         self.assertEqual(s.l, 1)
-        self.assertEqual(s.j, 1.5)
-        self.assertEqual(s.m, 0.5)
+        self.assertEqual(s.j, 2)
+        self.assertEqual(s.m, 0)
 
 class TestPairState(unittest.TestCase):
 
     def test_combination(self):
-        s1 = pi.StateOne("Rb", 80, 0, 0.5, 0.5)
-        s2 = pi.StateOne("Rb", 79, 1, 1.5, 0.5)
+        s1 = pi.StateOne("Sr3", 80, 2, 1, 0)
+        s2 = pi.StateOne("Sr3", 79, 1, 2, 0)
         s = pi.StateTwo(s1, s2)
         self.assertEqual(type(s), pi.StateTwo)
 
     def test_properties(self):
-        s1 = pi.StateOne("Rb", 80, 0, 0.5, 0.5)
-        s2 = pi.StateOne("Rb", 79, 1, 1.5, 0.5)
+        s1 = pi.StateOne("Sr3", 80, 2, 1, 0)
+        s2 = pi.StateOne("Sr3", 79, 1, 2, 0)
         s = pi.StateTwo(s1, s2)
 
         self.assertEqual(s.first(), s1)
         self.assertEqual(s.second(), s2)
         
-        self.assertEqual(s.element[0], "Rb")
-        self.assertEqual(s.element[1], "Rb")
+        self.assertEqual(s.species[0], "Sr3")
+        self.assertEqual(s.species[1], "Sr3")
+        self.assertEqual(s.element[0], "Sr")
+        self.assertEqual(s.element[1], "Sr")
+        self.assertEqual(s.s[0], 1)
+        self.assertEqual(s.s[1], 1)
         self.assertEqual(s.n[0], 80)
         self.assertEqual(s.n[1], 79)
-        self.assertEqual(s.l[0], 0)
+        self.assertEqual(s.l[0], 2)
         self.assertEqual(s.l[1], 1)
-        self.assertEqual(s.j[0], 0.5)
-        self.assertEqual(s.j[1], 1.5)
-        self.assertEqual(s.m[0], 0.5)
-        self.assertEqual(s.m[1], 0.5)
+        self.assertEqual(s.j[0], 1)
+        self.assertEqual(s.j[1], 2)
+        self.assertEqual(s.m[0], 0)
+        self.assertEqual(s.m[1], 0)
         
 if __name__ == '__main__':
     unittest.main()
