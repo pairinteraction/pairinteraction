a6d62d65652bae0f5889ad3e898948362407bfbc
Henri Menke <henri@icp.uni-stuttgart.de>
2018-07-04 14:55:54 +1200

    [travis] Perform automated static analysis

--- docker/build_cmake.sh
+++ docker/build_cmake.sh
@@ -10,6 +10,8 @@ if [ "$1" = "osx" ]; then
     cmake -DWITH_DMG=On -DCPACK_PACKAGE_FILE_NAME="${package}" ..;
 elif [ "$1" = "gcov" ]; then
     cmake -DWITH_COVERAGE=On -DCPACK_PACKAGE_FILE_NAME="${package}" ..;
+elif [ "$1" = "tidy" ]; then
+    cmake -DWITH_CLANG_TIDY=On -DWITH_GUI=Off -DCPACK_PACKAGE_FILE_NAME="${package}" ..;
 else
     cmake -DCPACK_PACKAGE_FILE_NAME="${package}" ..;
 fi;
