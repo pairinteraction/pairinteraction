1bfa5fd6cf7e5e880255b7f0338548412e747b9e
Sebastian Weber <seweber@posteo.de>
2018-07-26 17:30:04 +0200

    Fix power_kernel for Whittaker wave function

--- libpairinteraction/Wavefunction.h
+++ libpairinteraction/Wavefunction.h
@@ -206,7 +206,7 @@ public:
      * \param[in] power     exponent of r
      * \returns power kernel
      */
-    constexpr static inline double power_kernel(int power) { return 1.5 * power; }
+    constexpr static inline double power_kernel(int power) { return power+0.5; }
 };
 
 // --- Matrix element calculation ---
