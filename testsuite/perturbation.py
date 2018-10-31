dce8079c44367a3e1b736e908e8c44c33ce01558
Sebastian Weber <seweber@posteo.de>
2018-11-01 00:59:54 +0100

    [tests] Expand test of perturbative calculations

--- testsuite/perturbation.py
+++ testsuite/perturbation.py
@@ -21,7 +21,6 @@ class IntegrationTest(unittest.TestCase):
 
     def test_perturbation_zero_angle(self):
 
-        theta = 0
         distance = 5
         deltaN = 2
 
@@ -47,7 +46,6 @@ class IntegrationTest(unittest.TestCase):
         # System containing the perturbed Hamiltonian
         system_two_perturbed = pi.SystemTwo(system_two)
         system_two_perturbed.setDistance(distance)
-        system_two_perturbed.setAngle(theta)
 
         # Restrict unperturbed system to the subspace
         system_two_unperturbed.constrainBasisvectors(system_two_unperturbed.getBasisvectorIndex(state_two_subspace))
@@ -60,7 +58,7 @@ class IntegrationTest(unittest.TestCase):
 
         ### Perturbation theory up to second order ###
 
-        calculator = pi.PerturbativeInteraction(theta, self.cache)
+        calculator = pi.PerturbativeInteraction(self.cache)
 
         E0 = calculator.getEnergy(state_two_subspace)
         C3 = calculator.getC3(state_two_subspace)
@@ -125,6 +123,12 @@ class IntegrationTest(unittest.TestCase):
         ### Compare results ###
 
         np.testing.assert_allclose(hamiltonian_schrieffer_wolff.A, hamiltonian_second_order, rtol=1e-2)
+
+    def test_perturbation_C6(self):
+        calculator = pi.PerturbativeInteraction(self.cache)
+        C6 = calculator.getC6(pi.StateTwo(["Rb", "Rb"], [60, 60], [2, 2], [3 / 2, 3 / 2], [3 / 2, 3 / 2]), 4)
+
+        self.assertAlmostEqual(C6, -886.744, places=3)
 
 
 if __name__ == '__main__':
