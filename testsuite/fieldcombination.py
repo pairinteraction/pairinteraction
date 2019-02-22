eda070d59e4428d5c64716c795b1d9b0e4c0e056
Sebastian Weber <seweber@posteo.de>
2019-02-23 00:33:29 +0100

    [tests] Simplify test

--- testsuite/fieldcombination.py
+++ testsuite/fieldcombination.py
@@ -4,7 +4,7 @@ import shutil
 import tempfile
 import unittest
 
-from @LIBNAME@ import picomplex as pi
+from @LIBNAME@ import pireal as pi
 
 
 class FieldCombinationTest(unittest.TestCase):
@@ -16,7 +16,8 @@ class FieldCombinationTest(unittest.TestCase):
         system_one = pi.SystemOne("Rb", cache)
         system_one.restrictEnergy(-1077.243011609127, -939.9554235203701)
         system_one.restrictN(57, 63)
-        system_one.restrictL(-2, 3)
+        system_one.restrictL(0, 3)
+        system_one.setConservedMomentaUnderRotation([-0.5])
         system_one.setEfield((0, 0, 0.7))
         system_one.setBfield((0, 0, -8.8))
 
@@ -24,8 +25,8 @@ class FieldCombinationTest(unittest.TestCase):
         system_one.diagonalize()
 
         # Compare results
-        energies = np.real(system_one.getHamiltonian().diagonal()[[34, 54, 53]])
-        np.testing.assert_array_almost_equal(energies, [-1017.29997025, -1000.2551621, -1000.26792709], decimal=4)
+        energies = system_one.getHamiltonian().diagonal()
+        self.assertAlmostEqual(energies[13], -1000.26793709, places=4)
 
 
 if __name__ == '__main__':
