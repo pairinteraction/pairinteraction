73b9a1ea94e4ef466f346be2b6e52fd574b188ac
Sebastian Weber <seweber@posteo.de>
2019-03-06 23:53:39 +0100

    Upload to real pypi.org if on official repository, set up upload of wheels for windows

--- ci/deploy_pypi.sh
+++ ci/deploy_pypi.sh
@@ -3,5 +3,12 @@
 set -e;
 
 if [ -d $TRAVIS_BUILD_DIR/build/wheelhouse ]; then
-  twine upload --username pairinteraction-slave --password $PYPI_TOKEN --skip-existing --repository-url https://test.pypi.org/legacy/ $TRAVIS_BUILD_DIR/build/wheelhouse/*.whl;
+  case "$1" in
+    "real")
+      twine upload --username pairinteraction-slave --password $PYPI_TOKEN --skip-existing $TRAVIS_BUILD_DIR/build/wheelhouse/*.whl;
+      ;;
+    "test")
+      twine upload --username pairinteraction-slave --password $PYPI_TOKEN --skip-existing --repository-url https://test.pypi.org/legacy/ $TRAVIS_BUILD_DIR/build/wheelhouse/*.whl;
+      ;;
+  esac;
 fi
