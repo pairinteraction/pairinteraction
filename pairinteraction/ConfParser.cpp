10f8d67f597a4cd47dbbec949ad38b277d9e6c91
Henri Menke <henri@henrimenke.de>
2019-12-22 20:34:55 +1300

    [Boost] Replace <boost/property_tree/...>

--- pairinteraction/ConfParser.cpp
+++ pairinteraction/ConfParser.cpp
@@ -18,31 +18,64 @@
  */
 
 #include "ConfParser.hpp"
-#include <boost/property_tree/json_parser.hpp>
-#include <boost/property_tree/ptree.hpp>
+
+#include <cereal/external/rapidjson/document.h>
+#include <cereal/external/rapidjson/istreamwrapper.h>
+#include <cereal/external/rapidjson/ostreamwrapper.h>
+#include <cereal/external/rapidjson/writer.h>
+
+#include <fstream>
 #include <string>
 
 void Configuration::load_from_json(std::string const &filename) {
-    using boost::property_tree::ptree;
-    using boost::property_tree::json_parser::read_json;
+    std::ifstream ifs(filename);
 
-    ptree pt;
-    read_json(filename, pt);
+    rapidjson::IStreamWrapper isw(ifs);
+    rapidjson::Document document;
+    document.ParseStream<>(isw);
 
-    for (auto const &itr : pt) {
-        params[itr.first] << itr.second.data();
+    for (auto const &itr : document.GetObject()) {
+        std::string name = itr.name.GetString();
+        std::string value;
+        switch (itr.value.GetType()) {
+        case 0:
+            // empty string
+            break;
+        case 1:
+        case 2:
+            value = itr.value.GetBool() ? "true" : "false";
+            break;
+        case 5:
+            value = itr.value.GetString();
+            break;
+        case 6:
+            value = std::to_string(itr.value.GetDouble());
+            break;
+        default:
+            throw std::runtime_error("Can't convert value " + name);
+        };
+        params[name] << value;
     }
 }
 
 void Configuration::save_to_json(std::string const &filename) const {
-    using boost::property_tree::ptree;
-    using boost::property_tree::json_parser::write_json;
+    rapidjson::Document document;
 
-    ptree pt;
-
+    document.SetObject();
     for (auto const &p : *this) {
-        pt.put(p.first, p.second.str());
+        rapidjson::Value name;
+        name.SetString(p.first.c_str(), p.first.size(), document.GetAllocator());
+
+        rapidjson::Value value;
+        value.SetString(p.second.str().c_str(), p.second.str().size(), document.GetAllocator());
+
+        document.AddMember(name, value, document.GetAllocator());
     }
 
-    write_json(filename, pt);
+    std::ofstream ofs(filename);
+
+    rapidjson::OStreamWrapper osw(ofs);
+
+    rapidjson::Writer<rapidjson::OStreamWrapper> writer(osw);
+    document.Accept(writer);
 }
