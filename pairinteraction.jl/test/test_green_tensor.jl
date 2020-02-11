56ce26b9ddcc04c995d4fd13517a4b3d87b17dfd
Sebastian Weber <seweber@posteo.de>
2020-02-11 22:07:53 +0100

    Adapt license headers

--- pairinteraction.jl/test/test_green_tensor.jl
+++ pairinteraction.jl/test/test_green_tensor.jl
@@ -1,5 +1,21 @@
-# GreenTensorTest
+# Copyright (c) 2020 Sebastian Weber, Henri Menke, Alexander Papageorge. All rights reserved.
+# 
+# This file is part of the pairinteraction library.
+#
+# The pairinteraction library is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# The pairinteraction library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with the pairinteraction library. If not, see <http://www.gnu.org/licenses/>.
 
+# GreenTensorTest
 using Test
 using SparseArrays: dropzeros!
 using PairInteraction
