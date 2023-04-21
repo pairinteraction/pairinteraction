18a9d23d32fe9acabdd65c30325e6aafada46c94
Sebastian Weber <seweber@posteo.de>
2023-04-21 15:29:53 +0200

    Set localization of numbers explicitely to C to avoid caching problems, fixes the bug mentioned in #137

--- pairinteraction_gui/pairinteraction/unitmanagement.py
+++ pairinteraction_gui/pairinteraction/unitmanagement.py
@@ -14,11 +14,15 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with the pairinteraction GUI. If not, see <http://www.gnu.org/licenses/>.
+import locale
+
 from pint import UnitRegistry
 from scipy import constants
 
 
 # === Initialize unit registry ===
+
+locale.setlocale(locale.LC_NUMERIC, "C")
 
 ureg = UnitRegistry()
 Q = ureg.Quantity
