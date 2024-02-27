d73e989a2ee20edaa94e6ceb94dd7805d902598c
Sebastian Weber <seweber@posteo.de>
2024-02-27 16:59:47 +0100

    Build and test pairinteraction_backend

---
+++ pairinteraction_backend/basis/Basis.hpp
@@ -0,0 +1,11 @@
+#ifndef BASIS_HPP
+#define BASIS_HPP
+
+class Basis
+{
+public:
+    Basis();
+};
+
+
+#endif // BASIS_HPP
---
+++ pairinteraction_backend/main.cpp
@@ -0,0 +1,13 @@
+#define DOCTEST_CONFIG_IMPLEMENT
+#include <doctest/doctest.h>
+
+int main(int argc, char **argv) {
+
+  // Run the tests
+  doctest::Context ctx;
+  ctx.setOption("abort-after", 5);
+  ctx.setOption("no-run", 0);
+  ctx.applyCommandLine(argc, argv);
+  ctx.setOption("no-breaks", true);
+  return ctx.run();
+}
