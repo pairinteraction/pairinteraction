42bc5015917ca56cbd48d2cfee2341eeb77fa32f
Henri Menke <henri@henrimenke.de>
2024-02-29 20:53:31 +0100

    Python bindings for Ket

---
+++ pairinteraction_backend/binding.cpp
@@ -0,0 +1,7 @@
+#include <ket/Ket.py.hpp>
+
+#include <nanobind/nanobind.h>
+
+NB_MODULE(binding, m) {
+    bind_ket(m);
+}
---
+++ pairinteraction_backend/ket/Ket.py.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <nanobind/nanobind.h>
+
+void bind_ket(nanobind::module_ &m);
