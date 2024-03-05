ddadc7d3de765fe54201757a3bbcf08cf15169de
Philip Osterholz <philip.osterholz@uni-tuebingen.de>
2024-03-05 16:47:06 +0100

    filling in Ket

---
+++ pairinteraction_backend/ket/KetClassicalLight.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <string>
+
+#include "KetClassicalLightCreator.hpp"
+#include "ket/Ket.hpp"
+#include "ket/KetClassicalLightCreator.hpp"
+
+template <typename Real>
+class KetClassicalLightCreator;
+
+template <typename Real>
+class KetClassicalLight : public Ket<Real> {
+public:
+    int get_quantum_number_n_sigma_p() const;
+    int get_quantum_number_n_pi() const;
+    int get_quantum_number_n_sigma_m() const;
+
+private:
+    friend class KetClassicalLightCreator<Real>;
+    KetClassicalLight(Real energy, int n_sigma_p, int n_pi, int n_sigma_m,
+                      std::string label); // p can only be -1 for photons
+                                          // But this I handle in KetClassicalLightCreator.
+    int quantum_number_n_sigma_p;
+    int quantum_number_n_pi;
+    int quantum_number_n_sigma_m;
+};
+
+extern template class KetClassicalLight<float>;
+extern template class KetClassicalLight<double>;
---
+++ pairinteraction_backend/ket/KetClassicalLightCreator.cpp
@@ -0,0 +1,52 @@
+#include "ket/KetClassicalLightCreator.hpp"
+
+#include <exception>
+#include <limits>
+#include <string>
+
+template <typename Real>
+KetClassicalLightCreator<Real>::KetClassicalLightCreator() : parity(-1) {}
+
+template <typename Real>
+KetClassicalLightCreator<Real>::KetClassicalLightCreator(Real energy, int n_sigma_p, int n_pi,
+                                                         int n_sigma_m)
+    : energy(energy), quantum_number_n_sigma_p(n_sigma_p), quantum_number_n_pi(n_pi),
+      quantum_number_n_sigma_m(n_sigma_m), parity(-1) {}
+
+template <typename Real>
+KetClassicalLightCreator<Real> &KetClassicalLightCreator<Real>::set_energy(Real value) {
+    energy.emplace(value);
+    return *this;
+}
+
+template <typename Real>
+KetClassicalLightCreator<Real> &
+KetClassicalLightCreator<Real>::set_quantum_number_n_sigma_p(int value) {
+    quantum_number_n_sigma_p.emplace(value);
+    return *this;
+}
+
+template <typename Real>
+KetClassicalLightCreator<Real> &KetClassicalLightCreator<Real>::set_quantum_number_n_pi(int value) {
+    quantum_number_n_pi.emplace(value);
+    return *this;
+}
+
+template <typename Real>
+KetClassicalLightCreator<Real> &
+KetClassicalLightCreator<Real>::set_quantum_number_n_sigma_m(int value) {
+    quantum_number_n_sigma_m.emplace(value);
+    return *this;
+}
+
+template <typename Real>
+KetClassicalLight<Real> KetClassicalLightCreator<Real>::create() const {
+    return KetClassicalLight<Real>(
+        energy.value_or(std::numeric_limits<Real>::quiet_NaN()),
+        quantum_number_n_sigma_p.value_or(std::numeric_limits<int>::max()),
+        quantum_number_n_pi.value_or(std::numeric_limits<int>::max()),
+        quantum_number_n_sigma_m.value_or(std::numeric_limits<int>::max()), "my label");
+}
+
+template class KetClassicalLightCreator<float>;
+template class KetClassicalLightCreator<double>;
