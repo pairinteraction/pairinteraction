f5551305fd2e401b12ee071bf433f9eb480700a7
Sebastian Weber <seweber@posteo.de>
2024-04-12 16:30:30 +0200

    Use spdlog for logging

--- pairinteraction_backend/main.cpp
+++ pairinteraction_backend/main.cpp
@@ -1,7 +1,15 @@
 #define DOCTEST_CONFIG_IMPLEMENTATION_IN_DLL
 #include <doctest/doctest.h>
 
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
+
 int main(int argc, char **argv) {
+
+    // Configure a logger for the tests
+    auto doctest_logger = spdlog::stdout_color_mt("doctest");
+    doctest_logger->set_pattern(
+        "\033[36m[doctest]\033[0m [%Y-%m-%d %H:%M:%S.%e %t] [%^%l%$] [%s:%#] %v");
 
     // Run the tests
     doctest::Context ctx;
