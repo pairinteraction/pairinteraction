1e75331fbb92e119467f1b911f2003aa53ed1f15
Sebastian Weber <seweber@posteo.de>
2024-04-18 17:29:18 +0200

    Document how to do partial builds and use gdb

--- pairinteraction_backend/main.cpp
+++ pairinteraction_backend/main.cpp
@@ -8,8 +8,7 @@ int main(int argc, char **argv) {
 
     // Configure a logger for the tests
     auto doctest_logger = spdlog::stdout_color_mt("doctest");
-    doctest_logger->set_pattern(
-        "\033[36m[doctest]\033[0m [%Y-%m-%d %H:%M:%S.%e %t] [%^%l%$] [%s:%#] %v");
+    doctest_logger->set_pattern("[doctest] [%Y-%m-%d %H:%M:%S.%e %t] [%^%l%$] [%s:%#] %v");
 
     // Run the tests
     doctest::Context ctx;
