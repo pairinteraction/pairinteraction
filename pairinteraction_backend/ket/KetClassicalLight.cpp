63287eda494e5d3e1480063b835dda967a4ebb46
Philip Osterholz <philip.osterholz@uni-tuebingen.de>
2024-04-28 11:37:34 +0200

    [MW] removed seperate quantum numbers for polarizations

--- pairinteraction_backend/ket/KetClassicalLight.cpp
+++ pairinteraction_backend/ket/KetClassicalLight.cpp
@@ -2,11 +2,8 @@
 #include <limits>
 
 template <typename Real>
-KetClassicalLight<Real>::KetClassicalLight(Real energy, int n_sigma_p, int n_pi, int n_sigma_m,
-                                           size_t id)
-    : Ket<Real>(energy, std::numeric_limits<float>::max(), std::numeric_limits<float>::max(), -1),
-      id(id), quantum_number_n_sigma_p(n_sigma_p), quantum_number_n_pi(n_pi),
-      quantum_number_n_sigma_m(n_sigma_m) {}
+KetClassicalLight<Real>::KetClassicalLight(Real energy, int q, size_t id)
+    : Ket<Real>(energy, 0, 0, -1), id(id), quantum_number_q(q) {}
 
 template <typename Real>
 std::string KetClassicalLight<Real>::get_label() const {
@@ -29,18 +26,8 @@ KetClassicalLight<Real>::get_id_for_different_quantum_number_m(float new_quantum
 }
 
 template <typename Real>
-int KetClassicalLight<Real>::get_quantum_number_n_sigma_p() const {
-    return quantum_number_n_sigma_p;
-}
-
-template <typename Real>
-int KetClassicalLight<Real>::get_quantum_number_n_pi() const {
-    return quantum_number_n_pi;
-}
-
-template <typename Real>
-int KetClassicalLight<Real>::get_quantum_number_n_sigma_m() const {
-    return quantum_number_n_sigma_m;
+int KetClassicalLight<Real>::get_quantum_number_q() const {
+    return quantum_number_q;
 }
 
 template class KetClassicalLight<float>;
