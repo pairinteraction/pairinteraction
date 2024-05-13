27a9d427ffbe8c8292f7e976f00e12e67998caef
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2024-05-13 11:44:11 +0200

    fix test naming scheme

--- test/pairinteraction/test_simulation.py
+++ test/pairinteraction/test_simulation.py
@@ -13,44 +13,44 @@ names = ["simulation_1", "simulation_2", "simulation_3"]
 
 
 class SimulationTests(unittest.TestCase):
-    def testSimulations(self):
+    def test_simulations(self):
         for name in names:
-            self._oneTestSimulation(name)
+            self._one_test_simulation(name)
 
-    def loadModel(self, name):
+    def load_model(self, name):
         with open(f"{directory}/models/{name}.json", encoding="utf-8") as f:
             model = json.load(f)
         self.model = Model.model_validate(model)
 
-    def _oneTestSimulation(self, name):
-        self.loadModel(name)
+    def _one_test_simulation(self, name):
+        self.load_model(name)
         simulation = Simulation(self.model)
         results_list = simulation.run()
         energies_list = [results["energies"] for results in results_list]
 
         reference_path = f"{directory}/data/{name}__energies.txt"
         # use this for updating the reference
-        # self.saveEnergiesList(reference_path, energies_list)
+        # self.save_energies_list(reference_path, energies_list)
 
-        reference_list = self.loadEnergiesList(reference_path)
+        reference_list = self.load_energies_list(reference_path)
 
         assert len(energies_list) == len(reference_list)
         for energies, reference in zip(energies_list, reference_list):
             assert len(energies) == len(reference)
             assert np.allclose(energies, reference, rtol=1e-4)
 
-        self.removeCache()
+        self.remove_cache()
 
-    def saveEnergiesList(self, name, energies_list):
+    def save_energies_list(self, name, energies_list):
         with open(name, "w", encoding="utf-8") as f:
             for sublist in energies_list:
                 f.write(" ".join(map(str, sublist)) + "\n")
 
-    def loadEnergiesList(self, name):
+    def load_energies_list(self, name):
         with open(name, encoding="utf-8") as f:
             return [list(map(float, line.split())) for line in f]
 
-    def removeCache(self):
+    def remove_cache(self):
         if self.model.numerics.path_cache is not None:
             shutil.rmtree(self.model.numerics.path_cache, ignore_errors=True)
         self.model = None
