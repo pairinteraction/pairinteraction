2a020bfaee6eab6f7fa5bef0c99c7583bf0d6748
Henri Menke <henri@henrimenke.de>
2024-05-22 10:50:55 +0200

    [bindings] bind enums

--- src/cpp/bindings/enums/OperatorType.py.cpp
+++ src/cpp/bindings/enums/OperatorType.py.cpp
@@ -6,4 +6,13 @@
 
 namespace nb = nanobind;
 
-void bind_operator_type(nb::module_ &m) {}
+void bind_operator_type(nb::module_ &m) {
+    nb::enum_<OperatorType>(m, "OperatorType")
+        .value("ENERGY", OperatorType::ENERGY)
+        .value("ELECTRIC_DIPOLE", OperatorType::ELECTRIC_DIPOLE)
+        .value("ELECTRIC_QUADRUPOLE", OperatorType::ELECTRIC_QUADRUPOLE)
+        .value("ELECTRIC_OCTUPOLE", OperatorType::ELECTRIC_OCTUPOLE)
+        .value("MAGNETIC_DIPOLE", OperatorType::MAGNETIC_DIPOLE)
+        .value("DIAMAGNETIC", OperatorType::DIAMAGNETIC)
+        .value("ARBITRARY", OperatorType::ARBITRARY);
+}
