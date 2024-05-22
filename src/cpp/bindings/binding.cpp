1e8329eaf7e3a4b3375899fe91caf818287258d9
Henri Menke <henri@henrimenke.de>
2024-05-22 12:28:34 +0200

    [bindings] bind System*

--- src/cpp/bindings/binding.cpp
+++ src/cpp/bindings/binding.cpp
@@ -1,9 +1,37 @@
-#include "Ket.py.hpp"
-#include "pairinteraction/tools/setup.hpp"
+#include "basis/Basis.py.hpp"
+#include "database/Database.py.hpp"
+#include "enums/OperatorType.py.hpp"
+#include "enums/TransformationType.py.hpp"
+#include "interfaces/TransformationBuilderInterface.py.hpp"
+#include "ket/Ket.py.hpp"
+#include "operator/Operator.py.hpp"
+#include "system/System.py.hpp"
+#include "setup.hpp"
 
 #include <nanobind/nanobind.h>
 
-NB_MODULE(backend, m) { // NOLINT
-    pairinteraction::setup();
+NB_MODULE(pairinteraction_backend, m) {
+    setup();
+
+    // enums
+    bind_operator_type(m);
+    bind_transformation_type(m);
+
+    // interfaces
+    bind_transformation_builder_interface(m);
+
+    //operator
+    bind_operator(m);
+
+    // database
+    bind_database(m);
+
+    // ket
     bind_ket(m);
+
+    // basis
+    bind_basis(m);
+
+    // system
+    bind_system(m);
 }
