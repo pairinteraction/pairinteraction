94b2532ff042c0a3701629525a05a8baac28d703
Henri Menke <henri@henrimenke.de>
2024-05-22 10:50:55 +0200

    [bindings] bind enums

--- src/cpp/bindings/enums/TransformationType.py.cpp
+++ src/cpp/bindings/enums/TransformationType.py.cpp
@@ -6,4 +6,15 @@
 
 namespace nb = nanobind;
 
-void bind_transformation_type(nb::module_ &m) {}
+void bind_transformation_type(nb::module_ &m) {
+    nb::enum_<TransformationType>(m, "TransformationType", nb::is_arithmetic())
+        .value("NONE", TransformationType::NONE)
+        .value("SORT_BY_KET", TransformationType::SORT_BY_KET)
+        .value("SORT_BY_QUANTUM_NUMBER_F", TransformationType::SORT_BY_QUANTUM_NUMBER_F)
+        .value("SORT_BY_QUANTUM_NUMBER_M", TransformationType::SORT_BY_QUANTUM_NUMBER_M)
+        .value("SORT_BY_PARITY", TransformationType::SORT_BY_PARITY)
+        .value("SORT_BY_ENERGY", TransformationType::SORT_BY_ENERGY)
+        .value("ROTATE", TransformationType::ROTATE)
+        .value("ARBITRARY", TransformationType::ARBITRARY)
+        .value("MASK_SORTING", TransformationType::MASK_SORTING);
+}
