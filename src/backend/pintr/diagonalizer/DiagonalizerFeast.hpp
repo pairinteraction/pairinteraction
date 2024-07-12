031482deca927b40f1b4f851944a7b8a1056dfd3
Sebastian Weber <seweber@posteo.de>
2024-07-12 11:50:08 +0200

    Add the feast eigensolver

--- src/backend/pintr/diagonalizer/DiagonalizerFeast.hpp
+++ src/backend/pintr/diagonalizer/DiagonalizerFeast.hpp
@@ -12,9 +12,13 @@ class DiagonalizerFeast : public DiagonalizerInterface<Scalar> {
 public:
     using typename DiagonalizerInterface<Scalar>::real_t;
 
+    DiagonalizerFeast(int m0);
     EigenSystemH<Scalar> eigh(const Eigen::SparseMatrix<Scalar, Eigen::RowMajor> &matrix,
                               real_t min_eigenvalue, real_t max_eigenvalue,
                               int precision) const override;
+
+private:
+    int m0;
 };
 
 extern template class DiagonalizerFeast<float>;
