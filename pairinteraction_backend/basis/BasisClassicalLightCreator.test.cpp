8d242fddbb5667a8a12ee20ebf913e94938b09ec
Sebastian Weber <seweber@posteo.de>
2024-07-23 23:55:57 +0200

    Move unit tests to separate files

---
+++ pairinteraction_backend/basis/BasisClassicalLightCreator.test.cpp
@@ -0,0 +1,20 @@
+#include "basis/BasisClassicalLightCreator.hpp"
+
+#include "basis/BasisClassicalLight.hpp"
+#include "utils/streamed.hpp"
+
+#include <doctest/doctest.h>
+#include <spdlog/spdlog.h>
+
+DOCTEST_TEST_CASE("create a classical light basis") {
+    float energy{0.03};
+    auto basis = BasisClassicalLightCreator<float>()
+                     .set_photon_energy(energy)
+                     .restrict_quantum_number_q(-3, 3)
+                     .create();
+    for (auto ket : *basis) {
+        DOCTEST_CHECK(ket->get_photon_energy() == energy);
+        DOCTEST_CHECK(ket->get_energy() == ket->get_photon_energy() * ket->get_quantum_number_q());
+        SPDLOG_LOGGER_INFO(spdlog::get("doctest"), "Ket: {}", fmt::streamed(*ket));
+    }
+}
