7ace7fb80dd355420e735b8bba63cc8246cea722
Sebastian Weber <seweber@posteo.de>
2024-08-19 23:45:19 +0200

    Format CMakeLists files

--- cmake/FindLAPACKE.cmake
+++ cmake/FindLAPACKE.cmake
@@ -4,43 +4,48 @@ include(CheckCXXSymbolExists)
 find_package(LAPACK QUIET)
 
 if(LAPACK_FOUND)
-    # Some systems have LAPACKE in a separate library for some reason. The
-    # following attempts to detect that. Static linking is preferred.
-    find_library(LAPACKE_LIBRARY
-        NAMES liblapacke.a liblapacke.dylib lapacke
-        PATH_SUFFIXES lib
-        PATHS /usr/local/opt/lapack)
-    find_path(LAPACKE_INCLUDE_DIR
-        NAMES lapacke.h
-        PATH_SUFFIXES include
-        PATHS /usr/include/openblas /usr/local/opt/lapack)
+  # Some systems have LAPACKE in a separate library for some reason. The following attempts to detect that. Static
+  # linking is preferred.
+  find_library(
+    LAPACKE_LIBRARY
+    NAMES liblapacke.a liblapacke.dylib lapacke
+    PATH_SUFFIXES lib
+    PATHS /usr/local/opt/lapack)
+  find_path(
+    LAPACKE_INCLUDE_DIR
+    NAMES lapacke.h
+    PATH_SUFFIXES include
+    PATHS /usr/include/openblas /usr/local/opt/lapack)
 
-    set(CMAKE_REQUIRED_INCLUDES_SAVED ${CMAKE_REQUIRED_INCLUDES})
-    set(CMAKE_REQUIRED_LIBRARIES_SAVED ${CMAKE_REQUIRED_LIBRARIES})
-    if(LAPACKE_INCLUDE_DIR)
-        set(CMAKE_REQUIRED_INCLUDES "${LAPACKE_INCLUDE_DIR}")
-    endif()
-    if(LAPACKE_LIBRARY)
-        set(CMAKE_REQUIRED_LIBRARIES "${LAPACKE_LIBRARY};${LAPACK_LIBRARIES}")
-    else()
-        set(CMAKE_REQUIRED_LIBRARIES "${LAPACK_LIBRARIES}")
-    endif()
-    check_cxx_symbol_exists(LAPACKE_dsyevd "lapacke.h" LAPACKE_WORKS)
-    set(CMAKE_REQUIRED_INCLUDES ${CMAKE_REQUIRED_INCLUDES_SAVED})
-    set(CMAKE_REQUIRED_LIBRARIES ${CMAKE_REQUIRED_LIBRARIES_SAVED})
+  set(CMAKE_REQUIRED_INCLUDES_SAVED ${CMAKE_REQUIRED_INCLUDES})
+  set(CMAKE_REQUIRED_LIBRARIES_SAVED ${CMAKE_REQUIRED_LIBRARIES})
+  if(LAPACKE_INCLUDE_DIR)
+    set(CMAKE_REQUIRED_INCLUDES "${LAPACKE_INCLUDE_DIR}")
+  endif()
+  if(LAPACKE_LIBRARY)
+    set(CMAKE_REQUIRED_LIBRARIES "${LAPACKE_LIBRARY};${LAPACK_LIBRARIES}")
+  else()
+    set(CMAKE_REQUIRED_LIBRARIES "${LAPACK_LIBRARIES}")
+  endif()
+  check_cxx_symbol_exists(LAPACKE_dsyevd "lapacke.h" LAPACKE_WORKS)
+  set(CMAKE_REQUIRED_INCLUDES ${CMAKE_REQUIRED_INCLUDES_SAVED})
+  set(CMAKE_REQUIRED_LIBRARIES ${CMAKE_REQUIRED_LIBRARIES_SAVED})
 
-    if(LAPACKE_LIBRARY)
-        set_property(TARGET LAPACK::LAPACK PROPERTY INTERFACE_LINK_LIBRARIES "${LAPACKE_LIBRARY};${LAPACK_LIBRARIES}")
-    endif()
-    if(LAPACKE_INCLUDE_DIR)
-        set_property(TARGET LAPACK::LAPACK APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${LAPACKE_INCLUDE_DIR}")
-    endif()
+  if(LAPACKE_LIBRARY)
+    set_property(TARGET LAPACK::LAPACK PROPERTY INTERFACE_LINK_LIBRARIES "${LAPACKE_LIBRARY};${LAPACK_LIBRARIES}")
+  endif()
+  if(LAPACKE_INCLUDE_DIR)
+    set_property(
+      TARGET LAPACK::LAPACK
+      APPEND
+      PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${LAPACKE_INCLUDE_DIR}")
+  endif()
 
-    add_library(LAPACKE::LAPACKE ALIAS LAPACK::LAPACK)
+  add_library(LAPACKE::LAPACKE ALIAS LAPACK::LAPACK)
 endif()
 
-find_package_handle_standard_args(LAPACKE
-    VERSION_VAR LAPACK_VERSION
-    REQUIRED_VARS LAPACK_FOUND LAPACKE_WORKS
-    HANDLE_COMPONENTS
-)
+find_package_handle_standard_args(
+  LAPACKE
+  VERSION_VAR LAPACK_VERSION
+  REQUIRED_VARS LAPACK_FOUND LAPACKE_WORKS
+  HANDLE_COMPONENTS)
--- cmake/Findhttplib.cmake
+++ cmake/Findhttplib.cmake
@@ -2,39 +2,39 @@ include(FindPackageHandleStandardArgs)
 
 find_package(httplib QUIET CONFIG)
 if(httplib_CONSIDERED_CONFIGS)
-    set(target httplib::httplib)
+  set(target httplib::httplib)
 
-    find_package_handle_standard_args(httplib
-        HANDLE_COMPONENTS
-        CONFIG_MODE
-    )
+  find_package_handle_standard_args(httplib HANDLE_COMPONENTS CONFIG_MODE)
 else()
-    find_package(PkgConfig QUIET)
-    if(PKG_CONFIG_FOUND)
-        pkg_search_module(httplib QUIET IMPORTED_TARGET cpp-httplib)
-        if("-DCPPHTTPLIB_OPENSSL_SUPPORT" IN_LIST httplib_CFLAGS_OTHER)
-            set(httplib_OpenSSL_FOUND TRUE)
-        endif()
-        if("-DCPPHTTPLIB_ZLIB_SUPPORT" IN_LIST httplib_CFLAGS_OTHER)
-            set(httplib_ZLIB_FOUND TRUE)
-        endif()
-        if("-DCPPHTTPLIB_BROTLI_SUPPORT" IN_LIST httplib_CFLAGS_OTHER)
-            set(httplib_Brotli_FOUND TRUE)
-        endif()
-        set(target PkgConfig::httplib)
+  find_package(PkgConfig QUIET)
+  if(PKG_CONFIG_FOUND)
+    pkg_search_module(httplib QUIET IMPORTED_TARGET cpp-httplib)
+    if("-DCPPHTTPLIB_OPENSSL_SUPPORT" IN_LIST httplib_CFLAGS_OTHER)
+      set(httplib_OpenSSL_FOUND TRUE)
     endif()
-
-    find_package_handle_standard_args(httplib
-        REQUIRED_VARS httplib_FOUND
-        VERSION_VAR httplib_VERSION
-        HANDLE_COMPONENTS
-    )
-
-    if(httplib_FOUND)
-        add_library(httplib::httplib ALIAS PkgConfig::httplib)
+    if("-DCPPHTTPLIB_ZLIB_SUPPORT" IN_LIST httplib_CFLAGS_OTHER)
+      set(httplib_ZLIB_FOUND TRUE)
     endif()
+    if("-DCPPHTTPLIB_BROTLI_SUPPORT" IN_LIST httplib_CFLAGS_OTHER)
+      set(httplib_Brotli_FOUND TRUE)
+    endif()
+    set(target PkgConfig::httplib)
+  endif()
+
+  find_package_handle_standard_args(
+    httplib
+    REQUIRED_VARS httplib_FOUND
+    VERSION_VAR httplib_VERSION
+    HANDLE_COMPONENTS)
+
+  if(httplib_FOUND)
+    add_library(httplib::httplib ALIAS PkgConfig::httplib)
+  endif()
 endif()
 
 if(httplib_FOUND AND httplib_VERSION VERSION_GREATER_EQUAL 0.11.0)
-    set_property(TARGET ${target} APPEND PROPERTY INTERFACE_COMPILE_DEFINITIONS HTTPLIB_USES_STD_STRING)
+  set_property(
+    TARGET ${target}
+    APPEND
+    PROPERTY INTERFACE_COMPILE_DEFINITIONS HTTPLIB_USES_STD_STRING)
 endif()
