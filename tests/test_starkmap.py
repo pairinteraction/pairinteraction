ed9c5c919ba408cb29241dde6988b20057bb7040
Sebastian Weber <seweber@posteo.de>
2024-09-19 12:42:40 +0200

    Increase size of local database, also check eigenstates and kets in the starkmap test

--- tests/test_starkmap.py
+++ tests/test_starkmap.py
@@ -16,11 +16,14 @@ from pairinteraction.backend import (
     diagonalize,
 )
 
-reference_data_file = Path(__file__).parent.parent / "data/reference_stark_map/rb_small_basis.txt"
+reference_kets = Path(__file__).parent.parent / "data/reference_stark_map/kets.txt"
+reference_eigenvalues = Path(__file__).parent.parent / "data/reference_stark_map/eigenvalues.txt"
+reference_eigenstates = Path(__file__).parent.parent / "data/reference_stark_map/eigenstates.txt"
 
 
-def test_starkmap(ureg: UnitRegistry, generate_reference: bool, database_dir: Path, download_missing: bool) -> None:
+def test_starkmap(ureg: UnitRegistry, generate_reference: bool, database_dir: str, download_missing: bool) -> None:
     """Test calculating a Stark map."""
+    print(database_dir)
     database = Database(download_missing, True, database_dir)
     diagonalizer = DiagonalizerEigenDouble()
 
@@ -42,17 +45,22 @@ def test_starkmap(ureg: UnitRegistry, generate_reference: bool, database_dir: Pa
 
     # Diagonalize the systems in parallel
     diagonalize(systems, diagonalizer)
-    eigenvalues = [
-        (system.get_matrix().diagonal().real * ureg.hartree).to("gigahertz", "spectroscopy") for system in systems
-    ]
 
-    # Compare eigenvalues to reference values
+    # Compare to reference data
+    kets = [str(ket) for ket in systems[0].get_basis().get_kets()]
+    eigenvalues = [
+        (system.get_matrix().diagonal().real * ureg.hartree).to("gigahertz", "spectroscopy").magnitude
+        for system in systems
+    ]
+    eigenstates = [system.get_basis().get_coefficients().todense().A1 for system in systems]
+
     if generate_reference:
-        reference_data_file.parent.mkdir(parents=True, exist_ok=True)
-        np.savetxt(reference_data_file, [calc.magnitude for calc in eigenvalues])
-        print(f"Reference data generated and saved to {reference_data_file}")
+        reference_kets.parent.mkdir(parents=True, exist_ok=True)
+        np.savetxt(reference_kets, kets, fmt="%s", delimiter="\t")
+        np.savetxt(reference_eigenvalues, eigenvalues)
+        np.savetxt(reference_eigenstates, eigenstates)
         pytest.skip("Reference data generated, skipping comparison test")
 
-    reference_values = np.loadtxt(reference_data_file)
-    for calc, ref in zip(eigenvalues, reference_values):
-        np.testing.assert_allclose(calc.magnitude, ref)
+    np.testing.assert_equal(kets, np.loadtxt(reference_kets, dtype=str, delimiter="\t"))
+    np.testing.assert_allclose(eigenvalues, np.loadtxt(reference_eigenvalues))
+    np.testing.assert_allclose(eigenstates, np.loadtxt(reference_eigenstates))
