3d13e348321f89b6f63bec2c0ce60811bf908a4c
Sebastian Weber <seweber@posteo.de>
2024-10-24 11:48:54 +0200

    Use matrices for converting from cartesian to spherical coordinates [no ci]

--- src/cpp/include/pairinteraction/utils/spherical.hpp
+++ src/cpp/include/pairinteraction/utils/spherical.hpp
@@ -1,30 +1,43 @@
 #pragma once
 
+#include "pairinteraction/utils/eigen_assertion.hpp"
+#include "pairinteraction/utils/eigen_compat.hpp"
 #include "pairinteraction/utils/traits.hpp"
 
+#include <Eigen/Dense>
 #include <array>
+#include <complex>
 #include <limits>
 #include <stdexcept>
 
+using namespace std::complex_literals;
+
 namespace pairinteraction::spherical {
+
+extern const Eigen::Matrix<std::complex<double>, 3, 3> CARTESIAN_TO_SPHERICAL1;
+extern const Eigen::Matrix<std::complex<double>, 5, 9> CARTESIAN_TO_SPHERICAL2;
 
 template <typename Scalar>
 inline std::array<Scalar, 3>
 convert_to_spherical_basis(const std::array<typename traits::NumTraits<Scalar>::real_t, 3> &field) {
     using real_t = typename traits::NumTraits<Scalar>::real_t;
+    real_t numerical_precision = 10 * std::numeric_limits<real_t>::epsilon();
 
+    std::array<Scalar, 3> field_spherical;
     if constexpr (traits::NumTraits<Scalar>::is_complex_v) {
-        return {field[2],
-                std::complex<real_t>(-field[0], -field[1]) / static_cast<real_t>(std::sqrt(2)),
-                std::complex<real_t>(field[0], -field[1]) / static_cast<real_t>(std::sqrt(2))};
+        Eigen::Map<Eigen::Vector3<Scalar>>(field_spherical.data(), field_spherical.size()) =
+            CARTESIAN_TO_SPHERICAL1.template cast<Scalar>() *
+            Eigen::Map<const Eigen::Vector3<real_t>>(field.data(), field.size());
     } else {
-        if (std::abs(field[1]) > 10 * std::numeric_limits<real_t>::epsilon()) {
+        if (std::abs(field[1]) > numerical_precision) {
             throw std::invalid_argument(
                 "The field must not have a y-component if the scalar type is real.");
         }
-        return {field[2], -field[0] / static_cast<real_t>(std::sqrt(2)),
-                field[0] / static_cast<real_t>(std::sqrt(2))};
+        Eigen::Map<Eigen::Vector3<Scalar>>(field_spherical.data(), field_spherical.size()) =
+            CARTESIAN_TO_SPHERICAL1.real().template cast<Scalar>() *
+            Eigen::Map<const Eigen::Vector3<real_t>>(field.data(), field.size());
     }
-}
 
+    return field_spherical;
+}
 } // namespace pairinteraction::spherical
---
+++ src/cpp/src/utils/spherical.test.cpp
@@ -0,0 +1,28 @@
+#include "pairinteraction/utils/spherical.hpp"
+
+#include "pairinteraction/utils/streamed.hpp"
+
+#include <doctest/doctest.h>
+#include <spdlog/spdlog.h>
+
+namespace pairinteraction {
+DOCTEST_TEST_CASE("convert cartesian to spherical basis") {
+    DOCTEST_SUBCASE("kappa == 1") {
+        SPDLOG_LOGGER_DEBUG(spdlog::get("doctest"), "CARTESIAN_TO_SPHERICAL1:\n{}",
+                            fmt::streamed(spherical::CARTESIAN_TO_SPHERICAL1));
+
+        auto identity =
+            spherical::CARTESIAN_TO_SPHERICAL1 * spherical::CARTESIAN_TO_SPHERICAL1.adjoint();
+        DOCTEST_CHECK(identity.isApprox(Eigen::Matrix<double, 3, 3>::Identity(), 1e-9));
+    }
+
+    DOCTEST_SUBCASE("kappa == 2") {
+        SPDLOG_LOGGER_DEBUG(spdlog::get("doctest"), "CARTESIAN_TO_SPHERICAL2:\n{}",
+                            fmt::streamed(spherical::CARTESIAN_TO_SPHERICAL2));
+
+        auto identity = std::sqrt(2. / 3.) * spherical::CARTESIAN_TO_SPHERICAL2 *
+            std::sqrt(2. / 3.) * spherical::CARTESIAN_TO_SPHERICAL2.adjoint();
+        DOCTEST_CHECK(identity.isApprox(Eigen::Matrix<double, 5, 5>::Identity(), 1e-9));
+    }
+}
+} // namespace pairinteraction
