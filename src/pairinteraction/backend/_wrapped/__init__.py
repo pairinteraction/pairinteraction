71d346a5715dec38c2fbbbd542411d0323c08f77
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2024-11-19 16:41:38 +0100

    clean up _wrapped init

--- src/pairinteraction/backend/_wrapped/__init__.py
+++ src/pairinteraction/backend/_wrapped/__init__.py
@@ -1,53 +1,93 @@
 from pairinteraction.backend._wrapped.basis.BasisAtom import (
-    BasisAtomBase as BasisAtomBase,
-    BasisAtomComplexDouble as BasisAtomComplexDouble,
-    BasisAtomComplexFloat as BasisAtomComplexFloat,
-    BasisAtomDouble as BasisAtomDouble,
-    BasisAtomFloat as BasisAtomFloat,
+    BasisAtomBase,
+    BasisAtomComplexDouble,
+    BasisAtomComplexFloat,
+    BasisAtomDouble,
+    BasisAtomFloat,
 )
 from pairinteraction.backend._wrapped.basis.BasisCombined import (
-    BasisCombinedBase as BasisCombinedBase,
-    BasisCombinedComplexDouble as BasisCombinedComplexDouble,
-    BasisCombinedComplexFloat as BasisCombinedComplexFloat,
-    BasisCombinedDouble as BasisCombinedDouble,
-    BasisCombinedFloat as BasisCombinedFloat,
+    BasisCombinedBase,
+    BasisCombinedComplexDouble,
+    BasisCombinedComplexFloat,
+    BasisCombinedDouble,
+    BasisCombinedFloat,
 )
 from pairinteraction.backend._wrapped.calculate_electric_dipole_matrix_element import (
-    calculate_electric_dipole_matrix_element as calculate_electric_dipole_matrix_element,
+    calculate_electric_dipole_matrix_element,
 )
-from pairinteraction.backend._wrapped.calculate_energy import calculate_energy as calculate_energy
-from pairinteraction.backend._wrapped.Database import Database as Database
-from pairinteraction.backend._wrapped.diagonalize import diagonalize as diagonalize
-from pairinteraction.backend._wrapped.Diagonalizer import Diagonalizer as Diagonalizer
+from pairinteraction.backend._wrapped.calculate_energy import calculate_energy
+from pairinteraction.backend._wrapped.Database import Database
+from pairinteraction.backend._wrapped.diagonalize import diagonalize
+from pairinteraction.backend._wrapped.Diagonalizer import Diagonalizer
 from pairinteraction.backend._wrapped.ket.KetAtom import (
-    KetAtomBase as KetAtomBase,
-    KetAtomDouble as KetAtomDouble,
-    KetAtomFloat as KetAtomFloat,
+    KetAtomBase,
+    KetAtomDouble,
+    KetAtomFloat,
 )
 from pairinteraction.backend._wrapped.ket.KetClassicalLight import (
-    KetClassicalLightBase as KetClassicalLightBase,
-    KetClassicalLightDouble as KetClassicalLightDouble,
-    KetClassicalLightFloat as KetClassicalLightFloat,
+    KetClassicalLightBase,
+    KetClassicalLightDouble,
+    KetClassicalLightFloat,
 )
 from pairinteraction.backend._wrapped.ket.KetCombined import (
-    KetCombinedBase as KetCombinedBase,
-    KetCombinedComplexDouble as KetCombinedComplexDouble,
-    KetCombinedComplexFloat as KetCombinedComplexFloat,
-    KetCombinedDouble as KetCombinedDouble,
-    KetCombinedFloat as KetCombinedFloat,
+    KetCombinedBase,
+    KetCombinedComplexDouble,
+    KetCombinedComplexFloat,
+    KetCombinedDouble,
+    KetCombinedFloat,
 )
-from pairinteraction.backend._wrapped.Parity import Parity as Parity
+from pairinteraction.backend._wrapped.Parity import Parity
 from pairinteraction.backend._wrapped.system.SystemAtom import (
-    SystemAtomBase as SystemAtomBase,
-    SystemAtomComplexDouble as SystemAtomComplexDouble,
-    SystemAtomComplexFloat as SystemAtomComplexFloat,
-    SystemAtomDouble as SystemAtomDouble,
-    SystemAtomFloat as SystemAtomFloat,
+    SystemAtomBase,
+    SystemAtomComplexDouble,
+    SystemAtomComplexFloat,
+    SystemAtomDouble,
+    SystemAtomFloat,
 )
 from pairinteraction.backend._wrapped.system.SystemCombined import (
-    SystemCombinedBase as SystemCombinedBase,
-    SystemCombinedComplexDouble as SystemCombinedComplexDouble,
-    SystemCombinedComplexFloat as SystemCombinedComplexFloat,
-    SystemCombinedDouble as SystemCombinedDouble,
-    SystemCombinedFloat as SystemCombinedFloat,
+    SystemCombinedBase,
+    SystemCombinedComplexDouble,
+    SystemCombinedComplexFloat,
+    SystemCombinedDouble,
+    SystemCombinedFloat,
 )
+
+__all__ = [
+    "BasisAtomBase",
+    "BasisAtomComplexDouble",
+    "BasisAtomComplexFloat",
+    "BasisAtomDouble",
+    "BasisAtomFloat",
+    "BasisCombinedBase",
+    "BasisCombinedComplexDouble",
+    "BasisCombinedComplexFloat",
+    "BasisCombinedDouble",
+    "BasisCombinedFloat",
+    "calculate_electric_dipole_matrix_element",
+    "calculate_energy",
+    "Database",
+    "diagonalize",
+    "Diagonalizer",
+    "KetAtomBase",
+    "KetAtomDouble",
+    "KetAtomFloat",
+    "KetClassicalLightBase",
+    "KetClassicalLightDouble",
+    "KetClassicalLightFloat",
+    "KetCombinedBase",
+    "KetCombinedComplexDouble",
+    "KetCombinedComplexFloat",
+    "KetCombinedDouble",
+    "KetCombinedFloat",
+    "Parity",
+    "SystemAtomBase",
+    "SystemAtomComplexDouble",
+    "SystemAtomComplexFloat",
+    "SystemAtomDouble",
+    "SystemAtomFloat",
+    "SystemCombinedBase",
+    "SystemCombinedComplexDouble",
+    "SystemCombinedComplexFloat",
+    "SystemCombinedDouble",
+    "SystemCombinedFloat",
+]
