85088f96b18926a389129e30ed7fb02f189ac6f9
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2024-11-19 16:54:12 +0100

    format backend/float.py etc

--- src/pairinteraction/backend/complexfloat.py
+++ src/pairinteraction/backend/complexfloat.py
@@ -1,20 +1,17 @@
 from pairinteraction.backend._wrapped import (
-    # import objects without types (i.e. that are valid for all types)
+    BasisAtomComplexFloat as BasisAtom,
+    BasisCombinedComplexFloat as BasisCombined,
     Database,
     Diagonalizer,
+    KetAtomFloat as KetAtom,
+    KetClassicalLightFloat as KetClassicalLight,
+    KetCombinedComplexFloat as KetCombined,
     Parity,
+    SystemAtomComplexFloat as SystemAtom,
+    SystemCombinedComplexFloat as SystemCombined,
     calculate_electric_dipole_matrix_element,
     calculate_energy,
     diagonalize,
-    # import objects with only real types (i.e. either float or double)
-    KetAtomFloat as KetAtom,
-    KetClassicalLightFloat as KetClassicalLight,
-    # import objects with specific types (i.e. float, double, complexfloat or complexdouble)
-    BasisAtomComplexFloat as BasisAtom,
-    BasisCombinedComplexFloat as BasisCombined,
-    KetCombinedComplexFloat as KetCombined,
-    SystemAtomComplexFloat as SystemAtom,
-    SystemCombinedComplexFloat as SystemCombined,
 )
 
 __all__ = [
