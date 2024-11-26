eab44cba3be620259abbeeef3f0311a79a97246d
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2024-11-26 11:53:32 +0100

    improve global database

--- src/pairinteraction/backend/_wrapped/Database.py
+++ src/pairinteraction/backend/_wrapped/Database.py
@@ -23,12 +23,9 @@ class Database:
         database_dir: Union[str, "os.PathLike[str]"] = "",
     ) -> None:
         self._cpp = CPPDatabase(download_missing, wigner_in_memory, database_dir)
-
-    @classmethod
-    def _from_cpp_object(cls, cpp_obj: CPPDatabase) -> "Database":
-        obj = cls.__new__(cls)
-        obj._cpp = cpp_obj
-        return obj
+        self.download_missing = download_missing
+        self.wigner_in_memory = wigner_in_memory
+        self.database_dir = database_dir
 
     @classmethod
     def get_global_instance(
@@ -38,9 +35,18 @@ class Database:
         database_dir: Union[str, "os.PathLike[str]"] = "",
     ) -> "Database":
         if not hasattr(cls, "GlobalDatabase"):
-            cls.GlobalDatabase = cls._from_cpp_object(
-                CPPDatabase.get_global_instance(download_missing, wigner_in_memory, database_dir)  # type: ignore [reportUnkownMemberType]
-            )
+            obj = cls.__new__(cls)
+            obj._cpp = CPPDatabase.get_global_instance(download_missing, wigner_in_memory, database_dir)  # type: ignore [reportUnkownMemberType]
+            obj.download_missing = download_missing
+            obj.wigner_in_memory = wigner_in_memory
+            obj.database_dir = database_dir
+            cls.GlobalDatabase = obj
+        if (
+            cls.GlobalDatabase.download_missing != download_missing
+            or cls.GlobalDatabase.wigner_in_memory != wigner_in_memory
+            or cls.GlobalDatabase.database_dir != database_dir
+        ):
+            raise ValueError("Global instance already exists with different parameters")
         return cls.GlobalDatabase
 
     def get_matrix_elements(
