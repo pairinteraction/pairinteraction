702b43cce69e049d0d5e4c0cfb659512814d7a81
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2024-11-28 17:48:44 +0100

    Database add create_global_instance

--- src/pairinteraction/backend/_wrapped/Database.py
+++ src/pairinteraction/backend/_wrapped/Database.py
@@ -28,26 +28,22 @@ class Database:
         self.database_dir = database_dir
 
     @classmethod
-    def get_global_instance(
+    def get_global_instance(cls) -> "Database":
+        if not hasattr(cls, "GlobalDatabase"):
+            cls.create_global_instance()
+        return cls.GlobalDatabase
+
+    @classmethod
+    def create_global_instance(
         cls,
         download_missing: bool = False,
         wigner_in_memory: bool = True,
         database_dir: Union[str, "os.PathLike[str]"] = "",
-    ) -> "Database":
-        if not hasattr(cls, "GlobalDatabase"):
-            obj = cls.__new__(cls)
-            obj._cpp = CPPDatabase.get_global_instance(download_missing, wigner_in_memory, database_dir)  # type: ignore [reportUnkownMemberType]
-            obj.download_missing = download_missing
-            obj.wigner_in_memory = wigner_in_memory
-            obj.database_dir = database_dir
-            cls.GlobalDatabase = obj
-        if (
-            cls.GlobalDatabase.download_missing != download_missing
-            or cls.GlobalDatabase.wigner_in_memory != wigner_in_memory
-            or cls.GlobalDatabase.database_dir != database_dir
-        ):
-            raise ValueError("Global instance already exists with different parameters")
-        return cls.GlobalDatabase
+    ) -> None:
+        if hasattr(cls, "GlobalDatabase"):
+            raise ValueError("Global instance already exists")
+        # FIXME since CPPDatabase.get_global_instance is currently broken, we use a normal Database as global instance
+        cls.GlobalDatabase = cls(download_missing, wigner_in_memory, database_dir)
 
     def get_matrix_elements(
         self, basis_ket: "BasisAtomBase[Any]", basis_bra: "BasisAtomBase[Any]", operator: OperatorType, q: int
