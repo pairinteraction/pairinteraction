5c7ec6e8894efedb713dc57317c49485b1feae4f
Sebastian Weber <seweber@posteo.de>
2024-12-09 14:31:17 +0100

    Make exceptions thrown by eigen more meaningful by adding a backtrace

--- src/cpp/include/pairinteraction/utils/eigen_assertion.hpp
+++ src/cpp/include/pairinteraction/utils/eigen_assertion.hpp
@@ -1,13 +1,13 @@
 #pragma once
 
-#include <stdexcept>
+#include <cpptrace/cpptrace.hpp>
 
 #ifdef eigen_assert
 #error "'utils/eigen_assertion.hpp' must be included before any Eigen header"
 #else
 #define eigen_assert(X)                                                                            \
     if (!(X)) {                                                                                    \
-        throw std::runtime_error("<EIGEN> Assertion " EIGEN_MAKESTRING(                            \
+        throw cpptrace::runtime_error("<EIGEN> Assertion " EIGEN_MAKESTRING(                       \
             X) " failed at " __FILE__ ":" EIGEN_MAKESTRING(__LINE__));                             \
     }
 #endif
