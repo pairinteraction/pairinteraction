c9a3805093093805427146bc1ce2534dbfc9c73c
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2024-12-17 13:50:17 +0100

    diagonalize wrap energy range with unit

--- src/pairinteraction/backend/_wrapped/convenience/diagonalize.py
+++ src/pairinteraction/backend/_wrapped/convenience/diagonalize.py
@@ -2,9 +2,12 @@ from collections.abc import Sequence
 from typing import TYPE_CHECKING, Union
 
 from pairinteraction.backend import _backend
-from pairinteraction.backend._wrapped.Diagonalizer import Diagonalizer, get_cpp_diagonalizer
+from pairinteraction.backend._wrapped.Diagonalizer import Diagonalizer, get_cpp_diagonalizer, get_cpp_range
+from pairinteraction.units import QuantityScalar
 
 if TYPE_CHECKING:
+    from pint.facets.plain import PlainQuantity
+
     from pairinteraction.backend._wrapped.system.System import System
 
 
@@ -12,17 +15,22 @@ def diagonalize(
     systems: Sequence["System"],
     diagonalizer: Diagonalizer = "Eigen",
     precision: int = 12,
-    eigenvalue_range: Union[_backend.RangeFloat, _backend.RangeDouble, None] = None,
     sort_by_energy: bool = True,
+    energy_range: Union[tuple[float, float], tuple["PlainQuantity[float]", "PlainQuantity[float]"], None] = None,
+    energy_unit: str = "pint",
 ) -> None:
     cpp_systems = [s._cpp for s in systems]  # type: ignore [reportPrivateUsage]
-    diagonalize_fct = get_cpp_diagonalize(systems[0])
+    cpp_diagonalize_fct = get_cpp_diagonalize(systems[0])
 
     cpp_diagonalizer = get_cpp_diagonalizer(diagonalizer, cpp_systems[0])
-    if eigenvalue_range is None:
-        diagonalize_fct(cpp_systems, cpp_diagonalizer, precision)
+    if energy_range is None:
+        cpp_diagonalize_fct(cpp_systems, cpp_diagonalizer, precision)
     else:
-        diagonalize_fct(cpp_systems, cpp_diagonalizer, precision, eigenvalue_range)
+        min_energy_au = QuantityScalar(energy_range[0], energy_unit).to_base("ENERGY")
+        max_energy_au = QuantityScalar(energy_range[1], energy_unit).to_base("ENERGY")
+        cpp_range_class = get_cpp_range(cpp_systems[0])
+        cpp_range = cpp_range_class(min_energy_au, max_energy_au)
+        cpp_diagonalize_fct(cpp_systems, cpp_diagonalizer, precision, cpp_range)
     for system, cpp_system in zip(systems, cpp_systems):
         if sort_by_energy:
             sorter = cpp_system.get_sorter([_backend.TransformationType.SORT_BY_ENERGY])
