badc800cb8713bd9ff68acf1d9e6a418ca37d07a
Sebastian Weber <seweber@posteo.de>
2024-12-19 19:00:58 +0100

    Support all quantum numbers of the updated database format

--- src/cpp/include/pairinteraction/ket/KetAtom.hpp
+++ src/cpp/include/pairinteraction/ket/KetAtom.hpp
@@ -26,8 +26,9 @@ class KetAtom : public Ket<Real> {
 
 public:
     KetAtom(Private /*unused*/, Real energy, Real f, Real m, Parity p, std::string species, int n,
-            Real nu_exp, Real nu_std, Real l_exp, Real l_std, Real s_exp, Real s_std, Real j_exp,
-            Real j_std, bool is_j_total_momentum, bool is_calculated_with_mqdt, Database &database,
+            Real nu, Real nui_exp, Real nui_std, Real l_exp, Real l_std, Real s_exp, Real s_std,
+            Real j_exp, Real j_std, Real l_ryd_exp, Real l_ryd_std, Real j_ryd_exp, Real j_ryd_std,
+            bool is_j_total_momentum, bool is_calculated_with_mqdt, Database &database,
             size_t id_in_database);
 
     Database &get_database() const;
@@ -38,13 +39,18 @@ public:
     const std::string &get_species() const;
     int get_quantum_number_n() const;
     Real get_quantum_number_nu() const;
+    Real get_quantum_number_nui() const;
     Real get_quantum_number_l() const;
     Real get_quantum_number_s() const;
     Real get_quantum_number_j() const;
-    Real get_quantum_number_nu_std() const;
+    Real get_quantum_number_l_ryd() const;
+    Real get_quantum_number_j_ryd() const;
+    Real get_quantum_number_nui_std() const;
     Real get_quantum_number_l_std() const;
     Real get_quantum_number_s_std() const;
     Real get_quantum_number_j_std() const;
+    Real get_quantum_number_l_ryd_std() const;
+    Real get_quantum_number_j_ryd_std() const;
 
     bool operator==(const KetAtom<Real> &other) const;
     bool operator!=(const KetAtom<Real> &other) const;
@@ -56,14 +62,19 @@ public:
 private:
     std::string species;
     int quantum_number_n;
-    Real quantum_number_nu_exp;
-    Real quantum_number_nu_std;
+    Real quantum_number_nu;
+    Real quantum_number_nui_exp;
+    Real quantum_number_nui_std;
     Real quantum_number_l_exp;
     Real quantum_number_l_std;
     Real quantum_number_s_exp;
     Real quantum_number_s_std;
     Real quantum_number_j_exp;
     Real quantum_number_j_std;
+    Real quantum_number_l_ryd_exp;
+    Real quantum_number_l_ryd_std;
+    Real quantum_number_j_ryd_exp;
+    Real quantum_number_j_ryd_std;
     bool is_j_total_momentum;
     bool is_calculated_with_mqdt;
     Database &database;
--- src/cpp/src/ket/KetAtom.cpp
+++ src/cpp/src/ket/KetAtom.cpp
@@ -17,13 +17,16 @@ constexpr std::array<std::string_view, 6> quantum_number_l_labels = {"S", "P", "
 
 template <typename Real>
 KetAtom<Real>::KetAtom(Private /*unused*/, Real energy, Real f, Real m, Parity p,
-                       std::string species, int n, Real nu_exp, Real nu_std, Real l_exp, Real l_std,
-                       Real s_exp, Real s_std, Real j_exp, Real j_std, bool is_j_total_momentum,
+                       std::string species, int n, Real nu, Real nui_exp, Real nui_std, Real l_exp,
+                       Real l_std, Real s_exp, Real s_std, Real j_exp, Real j_std, Real l_ryd_exp,
+                       Real l_ryd_std, Real j_ryd_exp, Real j_ryd_std, bool is_j_total_momentum,
                        bool is_calculated_with_mqdt, Database &database, size_t id_in_database)
     : Ket<Real>(energy, f, m, p), species(std::move(species)), quantum_number_n(n),
-      quantum_number_nu_exp(nu_exp), quantum_number_nu_std(nu_std), quantum_number_l_exp(l_exp),
-      quantum_number_l_std(l_std), quantum_number_s_exp(s_exp), quantum_number_s_std(s_std),
-      quantum_number_j_exp(j_exp), quantum_number_j_std(j_std),
+      quantum_number_nu(nu), quantum_number_nui_exp(nui_exp), quantum_number_nui_std(nui_std),
+      quantum_number_l_exp(l_exp), quantum_number_l_std(l_std), quantum_number_s_exp(s_exp),
+      quantum_number_s_std(s_std), quantum_number_j_exp(j_exp), quantum_number_j_std(j_std),
+      quantum_number_l_ryd_exp(l_ryd_exp), quantum_number_l_ryd_std(l_ryd_std),
+      quantum_number_j_ryd_exp(j_ryd_exp), quantum_number_j_ryd_std(j_ryd_std),
       is_j_total_momentum(is_j_total_momentum), is_calculated_with_mqdt(is_calculated_with_mqdt),
       database(database), id_in_database(id_in_database) {}
 
@@ -58,7 +61,7 @@ std::string KetAtom<Real>::get_label() const {
 
     if (is_calculated_with_mqdt) {
         label += fmt::format("S={:.1f},nu={:.1f},L={:.1f},", quantum_number_s_exp,
-                             quantum_number_nu_exp, quantum_number_l_exp);
+                             quantum_number_nu, quantum_number_l_exp);
         label += this->is_j_total_momentum ? "J=" : "F=";
     } else {
         label += fmt::format("{:d},", quantum_number_n);
@@ -114,7 +117,12 @@ int KetAtom<Real>::get_quantum_number_n() const {
 
 template <typename Real>
 Real KetAtom<Real>::get_quantum_number_nu() const {
-    return quantum_number_nu_exp;
+    return quantum_number_nu;
+}
+
+template <typename Real>
+Real KetAtom<Real>::get_quantum_number_nui() const {
+    return quantum_number_nui_exp;
 }
 
 template <typename Real>
@@ -133,8 +141,18 @@ Real KetAtom<Real>::get_quantum_number_j() const {
 }
 
 template <typename Real>
-Real KetAtom<Real>::get_quantum_number_nu_std() const {
-    return quantum_number_nu_std;
+Real KetAtom<Real>::get_quantum_number_l_ryd() const {
+    return quantum_number_l_ryd_exp;
+}
+
+template <typename Real>
+Real KetAtom<Real>::get_quantum_number_j_ryd() const {
+    return quantum_number_j_ryd_exp;
+}
+
+template <typename Real>
+Real KetAtom<Real>::get_quantum_number_nui_std() const {
+    return quantum_number_nui_std;
 }
 
 template <typename Real>
@@ -153,11 +171,22 @@ Real KetAtom<Real>::get_quantum_number_j_std() const {
 }
 
 template <typename Real>
+Real KetAtom<Real>::get_quantum_number_l_ryd_std() const {
+    return quantum_number_l_ryd_std;
+}
+
+template <typename Real>
+Real KetAtom<Real>::get_quantum_number_j_ryd_std() const {
+    return quantum_number_j_ryd_std;
+}
+
+template <typename Real>
 bool KetAtom<Real>::operator==(const KetAtom<Real> &other) const {
     return Ket<Real>::operator==(other) && species == other.species &&
         quantum_number_n == other.quantum_number_n &&
-        quantum_number_nu_exp == other.quantum_number_nu_exp &&
-        quantum_number_nu_std == other.quantum_number_nu_std &&
+        quantum_number_nu == other.quantum_number_nu &&
+        quantum_number_nui_exp == other.quantum_number_nui_exp &&
+        quantum_number_nui_std == other.quantum_number_nui_std &&
         quantum_number_l_exp == other.quantum_number_l_exp &&
         quantum_number_l_std == other.quantum_number_l_std &&
         quantum_number_s_exp == other.quantum_number_s_exp &&
@@ -176,8 +205,9 @@ size_t KetAtom<Real>::hash::operator()(const KetAtom<Real> &k) const {
     size_t seed = typename Ket<Real>::hash()(k);
     utils::hash_combine(seed, k.species);
     utils::hash_combine(seed, k.quantum_number_n);
-    utils::hash_combine(seed, k.quantum_number_nu_exp);
-    utils::hash_combine(seed, k.quantum_number_nu_std);
+    utils::hash_combine(seed, k.quantum_number_nu);
+    utils::hash_combine(seed, k.quantum_number_nui_exp);
+    utils::hash_combine(seed, k.quantum_number_nui_std);
     utils::hash_combine(seed, k.quantum_number_l_exp);
     utils::hash_combine(seed, k.quantum_number_l_std);
     utils::hash_combine(seed, k.quantum_number_s_exp);
