8fb148018b66469f2406dabcb7c846ba97ba682d
Sebastian Weber <seweber@posteo.de>
2025-01-11 19:54:18 +0100

    Add a test case for receiving matrix elements from the database, update duckdb

---
+++ tests/test_db.py
@@ -0,0 +1,163 @@
+"""
+Test receiving matrix elements from the databases.
+"""
+
+from pathlib import Path
+
+import duckdb
+import numpy as np
+import pytest
+from packaging.version import Version
+from sympy.physics.wigner import wigner_3j
+
+import pairinteraction.backend.double as pi
+from tests.constants import GAUSS_IN_ATOMIC_UNITS, HARTREE_IN_GHZ
+
+expected_operator = np.array([[3.585881168022752, 1.664202126528398], [1.664202126528398, 2.664860530057922]])
+
+
+def fetch_id(n: int, l: float, f: float, s: float, connection: duckdb.duckdb.DuckDBPyConnection, table: str) -> int:
+    return connection.execute(
+        f"SELECT id FROM '{table}' WHERE n = {n} AND f = {f} ORDER BY (exp_s - {s})^2+(exp_l - {l})^2 LIMIT 1"
+    ).fetchone()[0]
+
+
+def fetch_wigner_element(
+    f_initial: float,
+    f_final: float,
+    m_initial: float,
+    m_final: float,
+    kappa: int,
+    q: int,
+    connection: duckdb.duckdb.DuckDBPyConnection,
+    table: str,
+) -> float:
+    result = connection.execute(
+        f"SELECT val FROM '{table}' WHERE f_initial = {f_initial} AND f_final = {f_final} AND "
+        f"m_initial = {m_initial} AND m_final = {m_final} AND kappa = {kappa} AND q = {q}"
+    ).fetchone()
+    return result[0] if result else 0
+
+
+def fetch_reduced_matrix_element(
+    id_initial: int, id_final: int, connection: duckdb.duckdb.DuckDBPyConnection, table: str
+) -> float:
+    result = connection.execute(
+        f"SELECT val FROM '{table}' WHERE id_initial = {id_initial} AND id_final = {id_final}"
+    ).fetchone()
+    return result[0] if result else 0
+
+
+@pytest.fixture(scope="module")
+def connection() -> duckdb.duckdb.DuckDBPyConnection:
+    with duckdb.connect(":memory:") as connection:
+        yield connection
+
+
+@pytest.fixture(scope="module")
+def database(database_dir: str, download_missing: bool) -> pi.Database:
+    return pi.Database(download_missing, True, database_dir)
+
+
+@pytest.mark.parametrize("swap_states", [False, True])
+def test_database(database: pi.Database, connection: duckdb.duckdb.DuckDBPyConnection, swap_states: bool) -> None:
+    """Test receiving matrix elements from the databases."""
+
+    bfield_in_gauss = 1500
+
+    # Define initial and final quantum states
+    n_initial, n_final = 54, 54
+    l_initial, l_final = 1, 1
+    f_initial, f_final = 1, 0
+    m_initial, m_final = 0, 0
+    s_initial, s_final = 0.6, 1.0
+
+    # Swap states if required by the test parameter
+    if swap_states:
+        n_initial, n_final = n_final, n_initial
+        l_initial, l_final = l_final, l_initial
+        f_initial, f_final = f_final, f_initial
+        m_initial, m_final = m_final, m_initial
+        s_initial, s_final = s_final, s_initial
+
+    # Get the Zeeman interaction operator from the database using pairinteraction
+    ket_initial = pi.KetAtom(
+        "Yb174_mqdt", n=n_initial, l=l_initial, f=f_initial, m=m_initial, s=s_initial, database=database
+    )
+    ket_final = pi.KetAtom("Yb174_mqdt", n=n_final, l=l_final, f=f_final, m=m_final, s=s_final, database=database)
+    basis = pi.BasisAtom("Yb174_mqdt", additional_kets=[ket_initial, ket_final], database=database)
+    operator = (
+        pi.SystemAtom(basis)
+        .set_magnetic_field([0, 0, bfield_in_gauss], unit="G")
+        .enable_diamagnetism(True)
+        .get_hamiltonian(unit="GHz")
+    )
+    operator -= np.diag(np.sort([ket_initial.get_energy(unit="GHz"), ket_final.get_energy(unit="GHz")]))
+    assert np.allclose(operator, expected_operator)
+
+    # Get the latest parquet files from the database directory
+    parquet_files = {}
+    for path in list(Path(database.database_dir).glob("*.parquet")):
+        name, version_str = path.stem.rsplit("_v", 1)
+        version = Version(version_str)
+        if name not in parquet_files or version > parquet_files[name].version:
+            parquet_files[name] = path
+    assert "wigner" in parquet_files.keys()
+    assert "Yb174_mqdt_states" in parquet_files.keys()
+    assert "Yb174_mqdt_matrix_elements_mu" in parquet_files.keys()
+    assert "Yb174_mqdt_matrix_elements_q" in parquet_files.keys()
+    assert "Yb174_mqdt_matrix_elements_q0" in parquet_files.keys()
+
+    # Obtain the ids of the initial and final states
+    id_initial = fetch_id(n_initial, l_initial, f_initial, s_initial, connection, parquet_files["Yb174_mqdt_states"])
+    assert id_initial == 310 if swap_states else 311
+
+    id_final = fetch_id(n_final, l_final, f_final, s_final, connection, parquet_files["Yb174_mqdt_states"])
+    assert id_final == 311 if swap_states else 310
+
+    # Obtain a matrix element of the magnetic dipole operator (for the chosen kets, it is non-zero iff initial != final)
+    kappa, q = 1, 0
+    wigner_element = fetch_wigner_element(
+        f_initial, f_final, m_initial, m_final, kappa, q, connection, parquet_files["wigner"]
+    )
+    assert np.isclose(
+        wigner_element,
+        float((-1) ** (f_final - m_final) * wigner_3j(f_final, kappa, f_initial, -m_final, q, m_initial)),
+    )
+
+    mu = fetch_reduced_matrix_element(id_initial, id_final, connection, parquet_files["Yb174_mqdt_matrix_elements_mu"])
+    matrix_element = -wigner_element * mu * bfield_in_gauss * GAUSS_IN_ATOMIC_UNITS * HARTREE_IN_GHZ
+    assert np.isclose(matrix_element, operator[0, 1])
+
+    # Obtain a matrix element of the diamagnetic operator (for the chosen kets, it is non-zero iff initial == final)
+    n_final = n_initial
+    l_final = l_initial
+    f_final = f_initial
+    m_final = m_initial
+    s_final = s_initial
+    id_final = id_initial
+
+    kappa, q = 0, 0
+    wigner_element = fetch_wigner_element(
+        f_initial, f_final, m_initial, m_final, kappa, q, connection, parquet_files["wigner"]
+    )
+    assert np.isclose(
+        wigner_element,
+        float((-1) ** (f_final - m_final) * wigner_3j(f_final, kappa, f_initial, -m_final, q, m_initial)),
+    )
+
+    q0 = fetch_reduced_matrix_element(id_initial, id_final, connection, parquet_files["Yb174_mqdt_matrix_elements_q0"])
+    matrix_element = 1 / 12 * wigner_element * q0 * (bfield_in_gauss * GAUSS_IN_ATOMIC_UNITS) ** 2 * HARTREE_IN_GHZ
+
+    kappa, q = 2, 0
+    wigner_element = fetch_wigner_element(
+        f_initial, f_final, m_initial, m_final, kappa, q, connection, parquet_files["wigner"]
+    )
+    assert np.isclose(
+        wigner_element,
+        float((-1) ** (f_final - m_final) * wigner_3j(f_final, kappa, f_initial, -m_final, q, m_initial)),
+    )
+
+    q = fetch_reduced_matrix_element(id_initial, id_final, connection, parquet_files["Yb174_mqdt_matrix_elements_q"])
+    matrix_element -= 1 / 12 * wigner_element * q * (bfield_in_gauss * GAUSS_IN_ATOMIC_UNITS) ** 2 * HARTREE_IN_GHZ
+    assert np.isclose(matrix_element, operator[0, 0] if swap_states else operator[1, 1])
