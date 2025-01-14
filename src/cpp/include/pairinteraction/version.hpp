4209fa75e9dc7e33198058b5d05a48e394d20493
Sebastian Weber <seweber@posteo.de>
2025-01-14 16:12:57 +0100

    bump version 1.1.2

--- src/cpp/include/pairinteraction/version.hpp
+++ src/cpp/include/pairinteraction/version.hpp
@@ -4,6 +4,6 @@ namespace pairinteraction {
 
 inline constexpr int VERSION_MAJOR = 1;
 inline constexpr int VERSION_MINOR = 1;
-inline constexpr int VERSION_PATCH = 1;
+inline constexpr int VERSION_PATCH = 2;
 
 } // namespace pairinteraction
