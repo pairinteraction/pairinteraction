1e09a872b99c6c35a622e25e189c6ca24722e970
Sebastian Weber <seweber@posteo.de>
2025-01-17 12:34:06 +0100

    Test obtaining kets

--- tests/constants.py
+++ tests/constants.py
@@ -2,6 +2,24 @@
 Constants used in the tests.
 """
 
+SUPPORTED_SPECIES = [
+    "Rb",
+    "Sr88_singlet",
+    "Sr88_triplet",
+    "Sr88_mqdt",
+    "Sr87_mqdt",
+    "Yb174_mqdt",
+    "Yb171_mqdt",
+    "Yb173_mqdt",
+]
+SPECIES_TO_NUCLEAR_SPIN = {
+    "Yb171_mqdt": 1 / 2,
+    "Yb173_mqdt": 5 / 2,
+    "Yb174_mqdt": 0,
+    "Sr87_mqdt": 9 / 2,
+    "Sr88_mqdt": 0,
+}
+
 HARTREE_IN_JOULES = 4.3597447222060e-18
 HARTREE_IN_GHZ = 6579683.920502
 HARTREE_IN_INVERSE_CM = 219474.63136320
--- tests/test_db.py
+++ tests/test_db.py
@@ -11,7 +11,7 @@ from packaging.version import Version
 from sympy.physics.wigner import wigner_3j
 
 import pairinteraction.backend.double as pi
-from tests.constants import GAUSS_IN_ATOMIC_UNITS, HARTREE_IN_GHZ
+from tests.constants import GAUSS_IN_ATOMIC_UNITS, HARTREE_IN_GHZ, SPECIES_TO_NUCLEAR_SPIN, SUPPORTED_SPECIES
 
 expected_operator = np.array([[3.58588117, 1.66420213], [1.66420213, 4.16645123]])
 
@@ -163,3 +163,40 @@ def test_database(database: pi.Database, connection: duckdb.duckdb.DuckDBPyConne
     q = fetch_reduced_matrix_element(id_initial, id_final, connection, parquet_files["Yb174_mqdt_matrix_elements_q"])
     matrix_element -= 1 / 12 * wigner_element * q * (bfield_in_gauss * GAUSS_IN_ATOMIC_UNITS) ** 2 * HARTREE_IN_GHZ
     assert np.isclose(matrix_element, operator[0, 0] if swap_states else operator[1, 1])
+
+
+@pytest.mark.parametrize("species", SUPPORTED_SPECIES)
+def test_obtaining_kets(database: pi.Database, species: str) -> None:
+    """Test obtaining kets from the database."""
+
+    is_mqdt = species.endswith("_mqdt")
+    is_single_valence_electron = species in ["Rb"]
+    is_triplet = species in ["Sr88_triplet"]
+
+    quantum_number_i = SPECIES_TO_NUCLEAR_SPIN[species] if is_mqdt else 0
+    quantum_number_s = 0.5 if is_single_valence_electron else (1 if is_triplet else 0)
+    quantum_number_f = quantum_number_i + quantum_number_s
+    quantum_number_m = quantum_number_i + quantum_number_s
+
+    # Obtain a ket from the database
+    ket = pi.KetAtom(species, n=60, l=0, f=quantum_number_f, m=quantum_number_m, s=quantum_number_s, database=database)
+
+    print(f"|{ket}>")
+
+    # Check the result
+    assert ket.species == species
+    assert ket.n == 60 if not is_mqdt else abs(ket.n - 60) < 1
+    assert ket.l == 0 if not is_mqdt else abs(ket.l - 0) < 1
+    assert ket.f == quantum_number_f
+    assert ket.m == quantum_number_m
+    assert ket.s == quantum_number_s if not is_mqdt else abs(ket.s - quantum_number_s) < 1
+    assert {
+        "Rb": "Rb:60,S_1/2,1/2",
+        "Sr88_singlet": "Sr88_singlet:60,S_0,0",
+        "Sr88_triplet": "Sr88_triplet:60,S_1,1",
+        "Sr88_mqdt": "Sr88:S=0.0,nu=56.7,L=0.0,J=0,0",
+        "Sr87_mqdt": "Sr87:S=0.5,nu=56.6,L=0.0,F=9/2,9/2",
+        "Yb174_mqdt": "Yb174:S=0.0,nu=55.7,L=0.0,J=0,0",
+        "Yb171_mqdt": "Yb171:S=0.3,nu=55.7,L=0.0,F=1/2,1/2",
+        "Yb173_mqdt": "Yb173:S=0.4,nu=56.4,L=0.0,F=5/2,5/2",
+    }[species] == str(ket)
