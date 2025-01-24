f63feee8b592d9cd5dd508900c6b0f6dc95f4181
Sebastian Weber <seweber@posteo.de>
2025-01-24 13:54:38 +0100

    Run a test with a downloaded database in the CI

--- tests/test_module_tests.py
+++ tests/test_module_tests.py
@@ -7,4 +7,4 @@ from pairinteraction import run_module_tests
 
 def test_module_tests(download_missing: bool, database_dir: str) -> None:
     """Execute the module tests."""
-    run_module_tests(download_missing, database_dir)
+    assert run_module_tests(download_missing, database_dir) == 0
