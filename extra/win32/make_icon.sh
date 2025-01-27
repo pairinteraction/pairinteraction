1c2cfaec4ed52651306efe30376b6bd67582a919
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2025-01-27 14:46:28 +0100

    remove extra

--- extra/win32/make_icon.sh
+++
@@ -1,5 +0,0 @@
-# SPDX-FileCopyrightText: 2024 Sebastian Weber, Henri Menke, and contributors. All rights reserved.
-# SPDX-License-Identifier: GPL-3.0-or-later OR LGPL-3.0-or-later
-
-pdflatex icon.tex
-convert -density 600 icon.pdf -define icon:auto-resize=256,48,32,16 pairinteraction.ico
