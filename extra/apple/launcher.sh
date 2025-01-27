a6d79bf847474fd401fc7c8495e9557f090b9059
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2025-01-27 14:46:28 +0100

    remove extra

--- extra/apple/launcher.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# http://stackoverflow.com/questions/26465240/how-to-execute-a-shell-script-from-within-an-app-and-show-it-in-the-terminal
-
-scriptPath=$(dirname "$0")'/../Resources/pairinteraction/gui/start_pairinteraction_gui.py'
-osascript -e 'tell app "Terminal" to do script "\"'"$scriptPath"'\"; exit"'
