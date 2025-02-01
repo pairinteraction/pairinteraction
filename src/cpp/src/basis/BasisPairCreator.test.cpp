308a340cd50c8e7f85e71a943382e32078187005
Sebastian Weber <seweber@posteo.de>
2025-02-01 19:03:45 +0100

    Add tests for BasisPair.get_matrix_elements

--- src/cpp/src/basis/BasisPairCreator.test.cpp
+++ src/cpp/src/basis/BasisPairCreator.test.cpp
@@ -5,12 +5,14 @@
 #include "pairinteraction/basis/BasisPair.hpp"
 #include "pairinteraction/database/Database.hpp"
 #include "pairinteraction/diagonalizer/DiagonalizerEigen.hpp"
+#include "pairinteraction/enums/OperatorType.hpp"
 #include "pairinteraction/enums/Parity.hpp"
 #include "pairinteraction/enums/TransformationType.hpp"
 #include "pairinteraction/ket/KetAtom.hpp"
 #include "pairinteraction/ket/KetAtomCreator.hpp"
 #include "pairinteraction/ket/KetPair.hpp"
 #include "pairinteraction/system/SystemAtom.hpp"
+#include "pairinteraction/system/SystemPair.hpp"
 #include "pairinteraction/utils/streamed.hpp"
 
 #include <doctest/doctest.h>
@@ -85,6 +87,108 @@ DOCTEST_TEST_CASE("create a BasisPair") {
     }
 }
 
-// TODO test obtaining operator matrix elements, for example, for "d+ x 1"
+DOCTEST_TEST_CASE("get matrix elements in the pair basis") {
+    DiagonalizerEigen<double> diagonalizer;
+
+    // Create single-atom system
+    Database &database = Database::get_global_instance();
+    auto basis = BasisAtomCreator<double>()
+                     .set_species("Rb")
+                     .restrict_quantum_number_n(58, 62)
+                     .restrict_quantum_number_l(0, 2)
+                     .create(database);
+    SystemAtom<double> system(basis);
+    system.set_electric_field({0, 0, 10 * 1.9446903811524456e-10});
+    system.diagonalize(diagonalizer);
+
+    // Get energy window for a two-atom basis
+    auto ket = KetAtomCreator<double>()
+                   .set_species("Rb")
+                   .set_quantum_number_n(60)
+                   .set_quantum_number_l(0)
+                   .set_quantum_number_m(0.5)
+                   .create(database);
+    double min_energy = 2 * ket->get_energy() - 3 / 6579683.920501762;
+    double max_energy = 2 * ket->get_energy() + 3 / 6579683.920501762;
+
+    // Create two-atom system
+    auto basis_pair_unperturbed = pairinteraction::BasisPairCreator<double>()
+                                      .add(system)
+                                      .add(system)
+                                      .restrict_energy(min_energy, max_energy)
+                                      .restrict_quantum_number_m(1, 1)
+                                      .create();
+    auto system_pair =
+        SystemPair<double>(basis_pair_unperturbed).set_distance(1e-6 / 5.29177210544e-11);
+    system_pair.diagonalize(diagonalizer);
+
+    auto basis_pair = system_pair.get_eigenbasis();
+
+    DOCTEST_SUBCASE("check dimensions") {
+        // <basis_pair_unperturbed|d0d0|basis_pair_unperturbed>
+        auto matrix_elements_all = basis_pair_unperturbed->get_matrix_elements(
+            basis_pair_unperturbed, OperatorType::ELECTRIC_DIPOLE, OperatorType::ELECTRIC_DIPOLE, 0,
+            0);
+        DOCTEST_CHECK(matrix_elements_all.rows() == basis_pair_unperturbed->get_number_of_states());
+        DOCTEST_CHECK(matrix_elements_all.cols() == basis_pair_unperturbed->get_number_of_states());
+
+        // <ket_pair|d0d0|basis_pair_unperturbed>
+        auto ket_pair = basis_pair_unperturbed->get_kets()[0];
+        auto matrix_elements_ket_pair = basis_pair_unperturbed->get_matrix_elements(
+            ket_pair, OperatorType::ELECTRIC_DIPOLE, OperatorType::ELECTRIC_DIPOLE, 0, 0);
+        DOCTEST_CHECK(matrix_elements_ket_pair.size() ==
+                      basis_pair_unperturbed->get_number_of_states());
+
+        {
+            Eigen::VectorX<double> ref = matrix_elements_all.row(0);
+            DOCTEST_CHECK(ref.isApprox(matrix_elements_ket_pair, 1e-11));
+        }
+
+        // <basis x basis|d0d0|basis_pair>
+        auto matrix_elements_product = basis_pair->get_matrix_elements(
+            basis, basis, OperatorType::ELECTRIC_DIPOLE, OperatorType::ELECTRIC_DIPOLE, 0, 0);
+        DOCTEST_CHECK(matrix_elements_product.rows() ==
+                      basis->get_number_of_states() * basis->get_number_of_states());
+        DOCTEST_CHECK(matrix_elements_product.cols() == basis_pair->get_number_of_states());
+
+        // <ket,ket|d0d0|basis_pair>
+        auto matrix_elements_ket = basis_pair->get_matrix_elements(
+            ket, ket, OperatorType::ELECTRIC_DIPOLE, OperatorType::ELECTRIC_DIPOLE, 0, 0);
+        DOCTEST_CHECK(matrix_elements_ket.size() == basis_pair->get_number_of_states());
+    }
+
+    DOCTEST_SUBCASE("check matrix elements") {
+        // energy
+        auto hamiltonian = basis_pair->get_matrix_elements(basis_pair, OperatorType::ENERGY,
+                                                           OperatorType::IDENTITY);
+        hamiltonian += basis_pair->get_matrix_elements(basis_pair, OperatorType::IDENTITY,
+                                                       OperatorType::ENERGY);
+
+        // interaction with electric field
+        {
+            Eigen::SparseMatrix<double, Eigen::RowMajor> tmp = -basis_pair->get_matrix_elements(
+                basis_pair, OperatorType::ELECTRIC_DIPOLE, OperatorType::IDENTITY, 0, 0);
+            tmp += -basis_pair->get_matrix_elements(basis_pair, OperatorType::IDENTITY,
+                                                    OperatorType::ELECTRIC_DIPOLE, 0, 0);
+            hamiltonian += 10 * 1.9446903811524456e-10 * tmp;
+        }
+
+        // dipole-dipole interaction
+        {
+            Eigen::SparseMatrix<double, Eigen::RowMajor> tmp = -2 *
+                basis_pair->get_matrix_elements(basis_pair, OperatorType::ELECTRIC_DIPOLE,
+                                                OperatorType::ELECTRIC_DIPOLE, 0, 0);
+            tmp += -basis_pair->get_matrix_elements(basis_pair, OperatorType::ELECTRIC_DIPOLE,
+                                                    OperatorType::ELECTRIC_DIPOLE, 1, -1);
+            tmp += -basis_pair->get_matrix_elements(basis_pair, OperatorType::ELECTRIC_DIPOLE,
+                                                    OperatorType::ELECTRIC_DIPOLE, -1, 1);
+            hamiltonian += std::pow(1e-6 / 5.29177210544e-11, -3) * tmp;
+        }
+
+        // compare to reference
+        auto ref = system_pair.get_matrix();
+        DOCTEST_CHECK(ref.isApprox(hamiltonian, 1e-11));
+    }
+}
 
 } // namespace pairinteraction
