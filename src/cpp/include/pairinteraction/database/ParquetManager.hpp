bd1bbd8780a5f9931e21a8ca731623208db5c8a5
Sebastian Weber <seweber@posteo.de>
2025-03-07 21:53:52 +0100

    Clean up

--- src/cpp/include/pairinteraction/database/ParquetManager.hpp
+++ src/cpp/include/pairinteraction/database/ParquetManager.hpp
@@ -17,41 +17,42 @@ class GitHubDownloader;
 
 class ParquetManager {
 public:
-    struct AssetInfo {
-        std::string path;
-        bool is_dir = false;
-        int version_minor = -1;
-    };
-
     struct PathInfo {
         std::string path;
         bool cached = false;
+    };
+
+    struct LocalAssetInfo {
+        int version_minor = -1;
+        std::unordered_map<std::string, PathInfo> paths;
+    };
+
+    struct RemoteAssetInfo {
+        int version_minor = -1;
+        std::string endpoint;
     };
 
     ParquetManager(std::filesystem::path directory, const GitHubDownloader &downloader,
                    std::vector<std::string> repo_paths, duckdb::Connection &con, bool use_cache);
     void scan_local();
     void scan_remote();
-    std::string get_path(const std::string &species, const std::string &table);
+    std::string get_path(const std::string &key, const std::string &table);
     std::string get_versions_info() const;
 
 private:
     void react_on_rate_limit_reached(std::time_t reset_time);
-    std::unordered_map<std::string, PathInfo>::iterator update_table(const std::string &species,
-                                                                     const std::string &table);
-    void cache_table(std::unordered_map<std::string, PathInfo>::iterator path_it);
+    void update_local_asset(const std::string &key);
+    void cache_table(std::unordered_map<std::string, PathInfo>::iterator table_it);
 
     std::filesystem::path directory_;
     const GitHubDownloader &downloader;
     std::vector<std::string> repo_paths_;
     duckdb::Connection &con;
     bool use_cache_;
-    std::unordered_map<std::string, AssetInfo> remote_asset_info;
-    std::unordered_map<std::string, AssetInfo> local_asset_info;
-    std::unordered_map<std::string, PathInfo> local_table_info;
-    std::regex parquet_regex{R"(^(\w+)_v(\d+)\.(\d+)\.parquet$)"};
-    std::regex zip_regex{R"(^(\w+)_v(\d+)\.(\d+)\.zip$)"};
-    std::regex dir_regex{R"(^(\w+)_v(\d+)\.(\d+)$)"};
+    std::unordered_map<std::string, LocalAssetInfo> local_asset_info;
+    std::unordered_map<std::string, RemoteAssetInfo> remote_asset_info;
+    std::regex local_regex{R"(^(\w+)_v(\d+)\.(\d+)$)"};
+    std::regex remote_regex{R"(^(\w+)_v(\d+)\.(\d+)\.zip$)"};
     std::shared_mutex mtx_local;
 };
 
