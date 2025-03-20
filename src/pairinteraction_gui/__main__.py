f0536f5bf589ad1716b9ef5570152ebc21d7080f
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2025-03-20 19:04:10 +0100

    [gui] add MainSignals

--- src/pairinteraction_gui/__main__.py
+++ src/pairinteraction_gui/__main__.py
@@ -1,4 +1,30 @@
-from pairinteraction_gui.app import main
+import logging
+import sys
+
+from pairinteraction_gui.app import Application
+from pairinteraction_gui.main_window import MainWindow
+
+
+def main() -> int:
+    """Run the PairInteraction GUI application.
+
+    Returns:
+        int: Application exit code
+
+    """
+    # in case we run the app from the terminal, add some logging
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+    app = Application(sys.argv)
+    app.setApplicationName("PairInteraction")
+
+    app.allow_ctrl_c()
+
+    window = MainWindow()
+    window.show()
+
+    return sys.exit(app.exec())
+
 
 if __name__ == "__main__":
     main()
