ffb62dba943d6fb005988b9e25c68d7382524a1b
Sebastian Weber <seweber@posteo.de>
2025-04-03 11:34:16 +0200

    Include also low-lying states in the databases for the unit tests, fix and improve the dbmanager

--- tools/dbmanager/src/dbmanager/__init__.py
+++ tools/dbmanager/src/dbmanager/__init__.py
@@ -1,6 +1,8 @@
 # SPDX-FileCopyrightText: 2024 Pairinteraction Developers
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
+# ruff: noqa: S608
+
 import argparse
 import logging
 import os
@@ -9,6 +11,7 @@ from dataclasses import dataclass
 from pathlib import Path
 
 import duckdb
+from packaging.version import Version
 
 logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(message)s", handlers=[logging.StreamHandler()])
 
@@ -19,7 +22,8 @@ logger = logging.getLogger(__name__)
 class TableFile:
     """Dataclass for storing the name, version, and path to a table of states or matrix elements."""
 
-    name: str
+    species: str
+    table: str
     version: int
     path: Path
 
@@ -43,27 +47,31 @@ def get_source_directory() -> Path:
 def load_parquet_and_csv_files(path_source: Path) -> dict[str, TableFile]:
     """Load the latest parquet files from the source directory."""
     parquet_and_csv_files: dict[str, TableFile] = {}
-    for path in list(path_source.glob("*.parquet")) + list(path_source.glob("*.csv")):
-        name, version_str = path.stem.rsplit("_v", 1)
-        version = int(version_str)
-        if name not in parquet_and_csv_files or version > parquet_and_csv_files[name].version:
-            parquet_and_csv_files[name] = TableFile(name, version, path)
-            logger.debug("Loaded parquet file: %s", parquet_and_csv_files[name])
+    for path in list(path_source.rglob("*.parquet")) + list(path_source.rglob("*.csv")):
+        species, version_str = path.parent.name.rsplit("_v", 1)
+        version = Version(version_str)
+        table = path.stem
+        key = f"{species}_{table}"
+        if key not in parquet_and_csv_files or version > parquet_and_csv_files[key].version:
+            parquet_and_csv_files[key] = TableFile(species, table, version, path)
+            logger.debug("Loaded parquet file: %s", parquet_and_csv_files[key])
     return parquet_and_csv_files
 
 
 def delete_old_parquet_and_csv_files(path_target: Path, parquet_and_csv_files: dict[str, TableFile]) -> None:
     """Delete old parquet files from the target directory."""
-    for path in list(path_target.glob("*.parquet")) + list(path_target.glob("*.csv")):
-        name, version_str = path.stem.rsplit("_v", 1)
-        version = int(version_str)
-        if name in parquet_and_csv_files:
-            if version < parquet_and_csv_files[name].version or parquet_and_csv_files[name].path.suffix == ".csv":
+    for path in list(path_target.rglob("*.parquet")) + list(path_target.rglob("*.csv")):
+        species, version_str = path.parent.name.rsplit("_v", 1)
+        version = Version(version_str)
+        table = path.stem
+        key = f"{species}_{table}"
+        if key in parquet_and_csv_files:
+            if version < parquet_and_csv_files[key].version or parquet_and_csv_files[key].path.suffix == ".csv":
                 path.unlink()
                 logger.info("Deleted csv / old parquet file: %s", path.name)
-            elif version > parquet_and_csv_files[name].version:
+            elif version > parquet_and_csv_files[key].version:
                 raise ValueError(
-                    f"Version of the table '{name}' in target directory is higher than in source directory."
+                    f"Version of the table '{key}' in target directory is higher than in source directory."
                 )
 
 
@@ -75,9 +83,11 @@ def write_parquet_files(
 ) -> None:
     """Write parquet files to the target directory. This also updates the path attribute of the parquet files."""
     for parquet_file in parquet_and_csv_files.values():
-        parquet_file.path = path_target / f"{parquet_file.name}_v{parquet_file.version}.parquet"
+        parquet_file.path = (
+            path_target / "tables" / f"{parquet_file.species}_v{parquet_file.version}" / f"{parquet_file.table}.parquet"
+        )
         copy_cmd = (
-            f"COPY {parquet_file.name} TO '{parquet_file.path}' "
+            f"COPY {parquet_file.species}_{parquet_file.table} TO '{parquet_file.path}' "
             f"(FORMAT PARQUET, {', '.join(f'{k} {v}' for k, v in options.items())})"
         )
         connection.execute(copy_cmd)
@@ -96,11 +106,11 @@ def print_metadata(connection: duckdb.DuckDBPyConnection, parquet_files: dict[st
             continue
 
         metadata = connection.execute(
-            "SELECT compression, row_group_num_rows FROM parquet_metadata('?')", parquet_file.path
+            f"SELECT compression, row_group_num_rows FROM parquet_metadata('{parquet_file.path}')"
         ).fetchone()
 
         file_metadata = connection.execute(
-            "SELECT num_rows, num_row_groups FROM parquet_file_metadata('?')", parquet_file.path
+            f"SELECT num_rows, num_row_groups FROM parquet_file_metadata('{parquet_file.path}')"
         ).fetchone()
 
         compression = metadata[0] if metadata else "Unknown"
@@ -112,7 +122,7 @@ def print_metadata(connection: duckdb.DuckDBPyConnection, parquet_files: dict[st
 
         table_data.append(
             [
-                f"{parquet_file.name}_v{parquet_file.version}",
+                f"{parquet_file.species}_v{parquet_file.version}/{parquet_file.table}",
                 f"{compression:12s}",
                 f"{num_rows:5.0f} k",
                 f"{num_row_groups:6d}",
@@ -175,8 +185,10 @@ def optimize() -> None:
 
         # Read parquet files into DuckDB
         for table_file in parquet_and_csv_files.values():
-            connection.execute("CREATE TEMP TABLE ? AS SELECT * FROM '?'", table_file.name, table_file.path)
-            logger.debug("Loaded table into DuckDB: %s", table_file.name)
+            connection.execute(
+                f"CREATE TEMP TABLE {table_file.species}_{table_file.table} AS SELECT * FROM '{table_file.path}'"
+            )
+            logger.debug("Loaded table into DuckDB: %s_%s", table_file.species, table_file.table)
 
         # Delete old parquet files from the target directory to keep it clean
         delete_old_parquet_and_csv_files(path_target, parquet_and_csv_files)
@@ -212,15 +224,26 @@ def shrink() -> None:
         default="ZSTD",
         help="The algorithm for compressing the parquet files.",
     )
-    parser.add_argument("--min_n", type=int, default=50, help="The minimum value for the quantum number n.")
-    parser.add_argument("--max_n", type=int, default=70, help="The maximum value for the quantum number n.")
+    parser.add_argument(
+        "--min_nu", type=int, default=50, help="The minimum value for the effective principal quantum number."
+    )
+    parser.add_argument(
+        "--max_nu", type=int, default=70, help="The maximum value for the effective principal quantum number."
+    )
+    parser.add_argument(
+        "--low_nu",
+        type=int,
+        default=20,
+        help="The value for the effective principal quantum number up to which low-lying states are included.",
+    )
     parser.add_argument("--max_f", type=int, default=5, help="The maximum value for the quantum number f.")
     args = parser.parse_args()
 
     path_target = args.out
     compression = args.compression
-    min_n = args.min_n
-    max_n = args.max_n
+    min_nu = args.min_nu
+    max_nu = args.max_nu
+    low_nu = args.low_nu
     max_f = args.max_f
 
     path_source = get_source_directory()
@@ -234,59 +257,72 @@ def shrink() -> None:
         logger.info("")
 
         # Read and filter parquet files
-        all_species: list[str] = []
-
-        if "wigner" in parquet_files:
-            parquet_file = parquet_files["wigner"]
-            connection.execute(
-                "CREATE TEMP TABLE ? AS SELECT * FROM '?' WHERE f_initial BETWEEN 0 AND ? AND f_final BETWEEN 0 AND ?",
-                parquet_file.name,
-                parquet_file.path,
-                max_f,
-                max_f,
-            )
-            logger.debug("Filtered 'wigner' table with max_f=%s", max_f)
+        for parquet_file in parquet_files.values():
+            if parquet_file.table == "wigner":
+                connection.execute(
+                    f"CREATE TEMP TABLE {parquet_file.species}_{parquet_file.table} AS "
+                    f"SELECT * FROM '{parquet_file.path}' "
+                    "WHERE f_initial BETWEEN 0 AND ? AND f_final BETWEEN 0 AND ?",
+                    [max_f, max_f],
+                )
+                logger.debug("Filtered '%s_%s' table with max_f=%s", parquet_file.species, parquet_file.table, max_f)
 
         for parquet_file in parquet_files.values():
-            if parquet_file.name.endswith("_states"):
-                species = parquet_file.name.rsplit("_", 1)[0]
-                all_species.append(species)
+            if parquet_file.table == "states":
                 connection.execute(
-                    "CREATE TEMP TABLE ? AS SELECT * FROM '?' WHERE nu BETWEEN ? AND ? AND f BETWEEN 0 AND ?",
-                    parquet_file.name,
-                    parquet_file.path,
-                    min_n,
-                    max_n,
-                    max_f,
+                    f"CREATE TEMP TABLE {parquet_file.species}_{parquet_file.table} AS "
+                    f"SELECT * FROM '{parquet_file.path}' "
+                    "WHERE (nu BETWEEN ? AND ? OR nu < ?) AND f BETWEEN 0 AND ?",
+                    [min_nu, max_nu, low_nu, max_f],
                 )
                 logger.debug(
-                    "Filtered '%s' with nu between %s and %s and f <= %s", parquet_file.name, min_n, max_n, max_f
+                    "Filtered '%s_%s' with nu between %s and %s or smaller %s and f <= %s",
+                    parquet_file.species,
+                    parquet_file.table,
+                    min_nu,
+                    max_nu,
+                    low_nu,
+                    max_f,
                 )
 
         for parquet_file in parquet_files.values():
-            if not parquet_file.name.endswith("_states") and parquet_file.name != "wigner":
-                species = next((s for s in all_species if parquet_file.name.startswith(s)), "")
-                if species:
-                    state_db_name = f"{species}_states"
-                    connection.execute(
-                        "CREATE TEMP TABLE ? AS "
-                        "WITH s AS (SELECT * FROM '?') "
-                        "SELECT s.* FROM s "
-                        "JOIN ? AS s1 ON s.id_initial = s1.id "
-                        "JOIN ? AS s2 ON s.id_final = s2.id",
-                        parquet_file.name,
-                        parquet_file.path,
-                        state_db_name,
-                        state_db_name,
-                    )
-                    logger.debug("Shrunk table '%s' based on '%s'", parquet_file.name, state_db_name)
+            if parquet_file.table not in ["wigner", "states"]:
+                state_db_name = f"{parquet_file.species}_states"
+                connection.execute(
+                    f"CREATE TEMP TABLE {parquet_file.species}_{parquet_file.table} AS "
+                    f"WITH s AS (SELECT * FROM '{parquet_file.path}') "
+                    "SELECT s.* FROM s "
+                    f"JOIN {state_db_name} AS s1 ON s.id_initial = s1.id "
+                    f"JOIN {state_db_name} AS s2 ON s.id_final = s2.id"
+                )
+                logger.debug(
+                    "Shrunk table '%s_%s' based on '%s'", parquet_file.species, parquet_file.table, state_db_name
+                )
 
-        # Remove parquet files for which a recent version exists in the target directory
-        for path in list(path_target.glob("*.parquet")) + list(path_target.glob("*.csv")):
-            name, version_str = path.stem.rsplit("_v", 1)
-            version = int(version_str)
-            if name in parquet_files and version > parquet_files[name].version:
-                parquet_files.pop(name)
+        # Remove parquet files from the list of parquet files if they do not belong to a species used for unit tests
+        used_species = [
+            "misc",
+            "Rb",
+            "Sr87_mqdt",
+            "Sr88_mqdt",
+            "Sr88_singlet",
+            "Sr88_triplet",
+            "Yb171_mqdt",
+            "Yb173_mqdt",
+            "Yb174_mqdt",
+        ]
+        keys_to_remove = [key for key, value in parquet_files.items() if value.species not in used_species]
+        for key in keys_to_remove:
+            parquet_files.pop(key)
+
+        # Remove parquet files from the list of parquet files if a recent version exists in the target directory
+        for path in list(path_target.rglob("*.parquet")) + list(path_target.rglob("*.csv")):
+            species, version_str = path.parent.name.rsplit("_v", 1)
+            version = Version(version_str)
+            table = path.stem
+            key = f"{species}_{table}"
+            if key in parquet_files and version > parquet_files[key].version:
+                parquet_files.pop(key)
 
         # Delete old parquet files from the target directory to keep it clean
         delete_old_parquet_and_csv_files(path_target, parquet_files)
