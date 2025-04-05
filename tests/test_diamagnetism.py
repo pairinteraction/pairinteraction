1a56af766f7184358ff793693f319b206086823e
Sebastian Weber <seweber@posteo.de>
2025-04-05 15:18:31 +0200

    Improve test of diamagnetism and rydberg-ion interaction

--- tests/test_diamagnetism.py
+++ tests/test_diamagnetism.py
@@ -5,14 +5,14 @@
 
 import numpy as np
 
-import pairinteraction.real as pi
+import pairinteraction.complex as pi
 
 
 def test_diamagnetism() -> None:
     """Test calculating diamagnetic interactions."""
     # Create a basis
-    ket = pi.KetAtom("Rb", n=60, l=0, m=0.5)
-    basis = pi.BasisAtom("Rb", n=(58, 62), l=(0, 2))
+    ket = pi.KetAtom("Rb", n=60, l=0, j=0.5, m=0.5)
+    basis = pi.BasisAtom("Rb", n=(ket.n - 2, ket.n + 2), l=(0, ket.l + 2))
     print(f"Number of basis states: {basis.number_of_states}")
 
     # Create system for a magnetic field of 1000 G
@@ -39,3 +39,22 @@ def test_diamagnetism() -> None:
     assert np.isclose(overlap, system.basis.get_corresponding_state(ket).get_overlaps(ket)[0])
     assert np.isclose(overlap, 0.9823116102876408, atol=1e-10)
     assert np.isclose(eigenenergy, 4.113262772909366)
+
+
+def test_diamagnetism_angle_dependence() -> None:
+    """Test calculating diamagnetic interactions for differently aligned magnetic fields."""
+    # Create a basis
+    basis = pi.BasisAtom("Rb", n=(58, 62), l=(0, 2))
+
+    # Create systems for fields in different directions
+    bfield = 1000
+    system_x = pi.SystemAtom(basis).set_magnetic_field([bfield, 0, 0], unit="G").set_diamagnetism_enabled(True)
+    system_y = pi.SystemAtom(basis).set_magnetic_field([0, bfield, 0], unit="G").set_diamagnetism_enabled(True)
+    system_z = pi.SystemAtom(basis).set_magnetic_field([0, 0, bfield], unit="G").set_diamagnetism_enabled(True)
+
+    # Diagonalize the systems in parallel
+    pi.diagonalize([system_x, system_y, system_z], diagonalizer="eigen", sort_by_energy=True)
+
+    # Ensure that all eigenenergies are the same
+    np.testing.assert_allclose(system_x.get_eigenenergies(unit="GHz"), system_y.get_eigenenergies(unit="GHz"))
+    np.testing.assert_allclose(system_x.get_eigenenergies(unit="GHz"), system_z.get_eigenenergies(unit="GHz"))
--- tests/test_ion.py
+++ tests/test_ion.py
@@ -4,6 +4,7 @@
 """Test atom-ion interaction."""
 
 import numpy as np
+import pytest
 
 import pairinteraction.complex as pi
 
@@ -11,27 +12,60 @@ import pairinteraction.complex as pi
 def test_ion() -> None:
     """Test the calculation of energy shifts in the field on an ion."""
     # Create a basis
+    ket = pi.KetAtom("Rb", n=60, l=0, j=0.5, m=0.5)
+    basis = pi.BasisAtom("Rb", n=(ket.n - 2, ket.n + 2), l=(0, ket.l + 2))
+
+    # Create systems for different distances to the ion
+    distance = 3
+    system_x = (
+        pi.SystemAtom(basis)
+        .set_ion_interaction_order(3)
+        .set_ion_charge(1, unit="e")
+        .set_ion_distance_vector([distance, 0, 0], unit="um")
+    )
+
+    # Diagonalize the system
+    system_x = system_x.diagonalize(diagonalizer="eigen", sort_by_energy=True)
+
+    # Ensure that values are correct for the system where the ion is closest to the atom
+    eigenenergies = system_x.get_eigenenergies(unit="GHz")
+    overlaps = system_x.basis.get_overlaps(ket)
+    idx = np.argmax(overlaps)
+    assert pytest.approx(overlaps[idx], rel=1e-12) == 0.8754157131466024  # NOSONAR
+    assert pytest.approx(eigenenergies[idx] - ket.get_energy(unit="GHz"), rel=1e-12) == -0.2531503909267485  # NOSONAR
+
+
+def test_ion_angle_dependence() -> None:
+    """Test the calculation of energy shifts in the field on an ion for different angles."""
+    # Create a basis
     basis = pi.BasisAtom("Rb", n=(58, 62), l=(0, 2))
 
     # Create systems for different distances to the ion
-    distances = np.linspace(1, 10, 5)
+    distances = np.linspace(3, 10, 5)
     systems_x = [
-        pi.SystemAtom(basis).set_ion_charge(1, unit="e").set_ion_distance_vector([d, 0, 0], unit="um")
+        pi.SystemAtom(basis)
+        .set_ion_interaction_order(3)
+        .set_ion_charge(1, unit="e")
+        .set_ion_distance_vector([d, 0, 0], unit="um")
         for d in distances
     ]
     systems_y = [
-        pi.SystemAtom(basis).set_ion_charge(1, unit="e").set_ion_distance_vector([0, d, 0], unit="um")
+        pi.SystemAtom(basis)
+        .set_ion_interaction_order(3)
+        .set_ion_charge(1, unit="e")
+        .set_ion_distance_vector([0, d, 0], unit="um")
         for d in distances
     ]
     systems_z = [
-        pi.SystemAtom(basis).set_ion_charge(1, unit="e").set_ion_distance_vector([0, 0, d], unit="um")
+        pi.SystemAtom(basis)
+        .set_ion_interaction_order(3)
+        .set_ion_charge(1, unit="e")
+        .set_ion_distance_vector([0, 0, d], unit="um")
         for d in distances
     ]
 
     # Diagonalize the systems in parallel
-    pi.diagonalize(systems_x, diagonalizer="eigen", sort_by_energy=True)
-    pi.diagonalize(systems_y, diagonalizer="eigen", sort_by_energy=True)
-    pi.diagonalize(systems_z, diagonalizer="eigen", sort_by_energy=True)
+    pi.diagonalize(systems_x + systems_y + systems_z, diagonalizer="eigen", sort_by_energy=True)
 
     # Ensure that all eigenenergies are the same
     for system_x, system_y, system_z in zip(systems_x, systems_y, systems_z):
