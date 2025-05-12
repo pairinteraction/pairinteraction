a4865a831e662da4cf9c778ef9419373afc0a5ff
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2025-05-12 10:53:54 +0200

    [gui] a few minor fixes

--- src/pairinteraction_gui/config/basis_config.py
+++ src/pairinteraction_gui/config/basis_config.py
@@ -218,12 +218,3 @@ class RestrictionsMQDT(RestrictionsBase):
         self.items["m"] = QnItemInt(
             self, "Δm", vdefault=5, tooltip="Restriction for the Magnetic quantum number m", checked=False
         )
-
-        # TODO this is not yet implemented
-        # self.items["l_ryd"] = QnItemDouble(
-        #     self,
-        #     "Δl_ryd",
-        #     vdefault=3,
-        #     tooltip="Restriction for the Orbital angular momentum l_ryd of the Rydberg electron",
-        #     checked=False,
-        # )
