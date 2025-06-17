b66ce02a5a7d73dfcc37bf70a09a46226773be52
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2025-06-17 15:15:44 +0200

    start adding c3 and c6

--- src/pairinteraction/perturbative/__init__.py
+++ src/pairinteraction/perturbative/__init__.py
@@ -1,6 +1,8 @@
 # SPDX-FileCopyrightText: 2025 Pairinteraction Developers
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
+from pairinteraction.perturbative.c3 import C3
+from pairinteraction.perturbative.c6 import C6
 from pairinteraction.perturbative.effective_system_pair import EffectiveSystemPair
 
-__all__ = ["EffectiveSystemPair"]
+__all__ = ["C3", "C6", "EffectiveSystemPair"]
