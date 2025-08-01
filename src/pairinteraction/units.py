cfb52fdbdb34f64b20eb76606015e8fa7869a79f
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2025-08-01 11:17:46 +0200

    add _pint_to_pint

--- src/pairinteraction/units.py
+++ src/pairinteraction/units.py
@@ -2,14 +2,27 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
 from collections.abc import Collection, Iterable
-from typing import Any, Generic, Literal, Optional, TypeVar, Union
+from typing import TYPE_CHECKING, Any, Generic, Literal, Optional, TypeVar, Union
 
-import numpy.typing as npt
+import pint
 from pint import UnitRegistry
-from pint.facets.numpy.quantity import NumpyQuantity
 from pint.facets.plain import PlainQuantity, PlainUnit
-from scipy.sparse import csr_matrix
 from typing_extensions import TypeAlias
+
+if TYPE_CHECKING:
+    import numpy.typing as npt
+    from pint.facets.numpy.quantity import NumpyQuantity
+    from scipy.sparse import csr_matrix
+
+    NDArray: TypeAlias = npt.NDArray[Any]
+    ArrayLike: TypeAlias = Union[npt.NDArray[Any], Collection[float]]
+    PintFloat: TypeAlias = PlainQuantity[float]
+    PintArray: TypeAlias = NumpyQuantity[NDArray]
+    PintArrayLike: TypeAlias = Union["PintArray", Collection[Union[float, PintFloat]]]
+    # type ignore here and also below for PlainQuantity[ValueType] because pint has no type support for scipy.csr_matrix
+    PintSparse: TypeAlias = NumpyQuantity[csr_matrix]  # type: ignore [type-var]
+    # and also for complex
+    PintComplex: TypeAlias = PlainQuantity[complex]  # type: ignore [type-var]
 
 ureg = UnitRegistry(system="atomic")
 
@@ -72,20 +85,11 @@ BaseContexts: dict[Dimension, Context] = {
     "c6": "spectroscopy",
 }
 
-NDArray: TypeAlias = npt.NDArray[Any]
-ArrayLike: TypeAlias = Union[npt.NDArray[Any], Collection[float]]
-PintFloat: TypeAlias = PlainQuantity[float]
-PintArray: TypeAlias = NumpyQuantity[NDArray]
-PintArrayLike: TypeAlias = Union["PintArray", Collection[Union[float, PintFloat]]]
-# type ignore here and also below for PlainQuantity[ValueType] because pint has no type support for scipy.csr_matrix
-PintSparse: TypeAlias = NumpyQuantity[csr_matrix]  # type: ignore [type-var]
-# and also for complex
-PintComplex: TypeAlias = PlainQuantity[complex]  # type: ignore [type-var]
-
 ValueType = TypeVar("ValueType", bound=Union[float, "NDArray", "csr_matrix"])
 PintType = TypeVar("PintType", bound=Union["PintFloat", "PintArray", "PintSparse"])  # type: ignore [type-var]
 ValueTypeLike = TypeVar("ValueTypeLike", bound=Union[float, "ArrayLike", "csr_matrix"])
 PintTypeLike = TypeVar("PintTypeLike", bound=Union["PintFloat", "PintArrayLike", "PintSparse"])
+T = TypeVar("T", bound=PlainQuantity[Any])
 
 
 class UnitConverterGeneric(Generic[ValueType, PintType, ValueTypeLike, PintTypeLike]):
@@ -102,6 +106,25 @@ class UnitConverterGeneric(Generic[ValueType, PintType, ValueTypeLike, PintTypeL
             return [BaseContexts[dimension]] if dimension in BaseContexts else []
         contexts: set[Context] = {BaseContexts[d] for d in dimension if d in BaseContexts}
         return list(contexts)
+
+    @staticmethod
+    def _pint_to_pint(
+        quantity: T,
+        unit: str,
+        dimension: DimensionLike,
+    ) -> T:
+        """Convert a pint.Quantity to another pint.Quantity with a different unit."""
+        contexts = UnitConverterGeneric._get_contexts_from_dimension(dimension)
+        try:
+            return quantity.to(unit, *contexts)  # type: ignore [return-value]
+        except pint.errors.DimensionalityError:
+            # pint uses e.g. the context "spectroscopy" to convert "hartree" -> "GHz"
+            # however, something like "hartree * bohr^3" -> "GHz * bohr^3" does not work
+            # the following is a workaround for this kind of conversions
+            if "spectroscopy" in contexts:
+                quantity = quantity * ureg.Quantity(1, "GHz") / ureg.Quantity(1, "GHz").to("hartree", "spectroscopy")
+                return quantity.to(unit, *contexts)  # type: ignore [return-value]
+            raise
 
     @classmethod
     def au_to_pint(
@@ -122,7 +145,6 @@ class UnitConverterGeneric(Generic[ValueType, PintType, ValueTypeLike, PintTypeL
     ) -> PintType:
         """Convert a user-defined value to a pint.Quantity."""
         unit_au = cls._get_unit_au_from_dimension(dimension)
-        contexts = cls._get_contexts_from_dimension(dimension)
         quantity: PintType
         if isinstance(qty_val, PlainQuantity):
             if unit is not None:
@@ -136,7 +158,8 @@ class UnitConverterGeneric(Generic[ValueType, PintType, ValueTypeLike, PintTypeL
                     if qv == 0:
                         new_qty_val.append(0)
                     elif isinstance(qv, PlainQuantity):
-                        new_qty_val.append(qv.to(unit_au, *contexts).magnitude)
+                        value = cls._pint_to_pint(qv, unit_au, dimension).magnitude
+                        new_qty_val.append(value)
                     else:
                         raise ValueError("unit must be given if qty_val is not a pint.Quantity and non-zero")
                 qty_val = new_qty_val  # type: ignore [assignment]
@@ -145,7 +168,7 @@ class UnitConverterGeneric(Generic[ValueType, PintType, ValueTypeLike, PintTypeL
                 if qty_val != 0:
                     raise ValueError("unit must be given if qty_val is not a pint.Quantity")
             quantity = ureg.Quantity(qty_val, unit)  # type: ignore [assignment]
-        return quantity.to(unit_au, *contexts)  # type: ignore [return-value]
+        return cls._pint_to_pint(quantity, unit_au, dimension)
 
     @classmethod
     def pint_to_au(
@@ -155,8 +178,7 @@ class UnitConverterGeneric(Generic[ValueType, PintType, ValueTypeLike, PintTypeL
     ) -> ValueType:
         """Convert a pint.Quantity to a value in atomic units."""
         unit_au = cls._get_unit_au_from_dimension(dimension)
-        contexts = cls._get_contexts_from_dimension(dimension)
-        return quantity.to(unit_au, *contexts).magnitude  # type: ignore [no-any-return]
+        return cls._pint_to_pint(quantity, unit_au, dimension).magnitude  # type: ignore [return-value]
 
     @classmethod
     def user_to_au(
@@ -188,21 +210,19 @@ class UnitConverterGeneric(Generic[ValueType, PintType, ValueTypeLike, PintTypeL
         to_unit: Optional[str],
     ) -> Union[PintType, ValueType]:
         """Convert a pint.Quantity to a user-defined value."""
-        contexts = cls._get_contexts_from_dimension(dimension)
         if to_unit is None:
             unit_au = cls._get_unit_au_from_dimension(dimension)
-            return quantity.to(unit_au, *contexts)  # type: ignore [return-value]
-        return quantity.to(to_unit, *contexts).magnitude  # type: ignore [no-any-return]
+            return cls._pint_to_pint(quantity, unit_au, dimension)
+        return cls._pint_to_pint(quantity, to_unit, dimension).magnitude  # type: ignore [return-value]
 
 
-
-class UnitConverterScalar(UnitConverterGeneric[float, PintFloat, float, PintFloat]):
+class UnitConverterScalar(UnitConverterGeneric[float, "PintFloat", float, "PintFloat"]):
     """Unit converter for scalar values."""
 
 
-class UnitConverterArray(UnitConverterGeneric[NDArray, PintArray, ArrayLike, PintArrayLike]):
+class UnitConverterArray(UnitConverterGeneric["NDArray", "PintArray", "ArrayLike", "PintArrayLike"]):
     """Unit converter for array values."""
 
 
-class UnitConverterSparse(UnitConverterGeneric[csr_matrix, PintSparse, csr_matrix, PintSparse]):
+class UnitConverterSparse(UnitConverterGeneric["csr_matrix", "PintSparse", "csr_matrix", "PintSparse"]):
     """Unit converter for sparse matrix values."""
