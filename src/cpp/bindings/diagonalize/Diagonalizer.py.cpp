fd8434ac7d80139c1776af5faebfd64c49b6545d
Henri Menke <henri@henrimenke.de>
2025-08-10 19:54:26 +0200

    Diagonalizer: avoid copies of systems

--- src/cpp/bindings/diagonalize/Diagonalizer.py.cpp
+++ src/cpp/bindings/diagonalize/Diagonalizer.py.cpp
@@ -79,15 +79,12 @@ static void declare_diagonalize(nb::module_ &m, std::string const &type_name) {
            const DiagonalizerInterface<scalar_t> &diagonalizer,
            std::optional<real_t> min_eigenvalue, std::optional<real_t> max_eigenvalue,
            double rtol) {
-            std::vector<T> systems;
+            std::vector<std::reference_wrapper<T>> systems;
             systems.reserve(pylist.size());
-            for (auto h : pylist) {
-                systems.push_back(nb::cast<T>(h));
+            for (nb::handle_t<T> &&h : pylist) {
+                systems.push_back(nb::cast<T &>(h));
             }
             diagonalize(systems, diagonalizer, min_eigenvalue, max_eigenvalue, rtol);
-            for (size_t i = 0; i < systems.size(); ++i) {
-                pylist[i] = nb::cast(systems[i]);
-            }
         },
         "systems"_a, "diagonalizer"_a, "min_eigenvalue"_a = nb::none(),
         "max_eigenvalue"_a = nb::none(), "rtol"_a = 1e-6);
