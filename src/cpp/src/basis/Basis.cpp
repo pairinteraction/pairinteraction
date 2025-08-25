f5ce52738941b7dd7b56c8819e0f1820b2260809
Sebastian Weber <seweber@posteo.de>
2025-08-25 15:43:23 +0200

    Fix segfault from invalid mapping

--- src/cpp/src/basis/Basis.cpp
+++ src/cpp/src/basis/Basis.cpp
@@ -244,12 +244,16 @@ std::shared_ptr<const Derived> Basis<Derived>::get_state(size_t state_index) con
 
     std::fill(restricted->ket_index_to_state_index.begin(),
               restricted->ket_index_to_state_index.end(), std::numeric_limits<int>::max());
-    restricted->ket_index_to_state_index[state_index_to_ket_index[state_index]] = 0;
+
+    size_t ket_index = state_index_to_ket_index[state_index];
+    restricted->state_index_to_ket_index = {ket_index};
+    if (ket_index != std::numeric_limits<int>::max()) {
+        restricted->ket_index_to_state_index[ket_index] = 0;
+    }
 
     restricted->state_index_to_quantum_number_f = {state_index_to_quantum_number_f[state_index]};
     restricted->state_index_to_quantum_number_m = {state_index_to_quantum_number_m[state_index]};
     restricted->state_index_to_parity = {state_index_to_parity[state_index]};
-    restricted->state_index_to_ket_index = {state_index_to_ket_index[state_index]};
 
     restricted->_has_quantum_number_f =
         restricted->state_index_to_quantum_number_f[0] != std::numeric_limits<real_t>::max();
@@ -305,7 +309,7 @@ size_t Basis<Derived>::get_corresponding_state_index(std::shared_ptr<const ket_t
 
 template <typename Derived>
 size_t Basis<Derived>::get_corresponding_ket_index(size_t state_index) const {
-    int ket_index = state_index_to_ket_index.at(state_index);
+    size_t ket_index = state_index_to_ket_index.at(state_index);
     if (ket_index == std::numeric_limits<int>::max()) {
         throw std::runtime_error("The state does not belong to a ket in a well-defined way.");
     }
@@ -588,7 +592,8 @@ void Basis<Derived>::get_indices_of_blocks_without_checks(
                     break;
                 }
             } else if (label == TransformationType::SORT_BY_KET) {
-                if (state_index_to_ket_index[i] != last_ket) {
+                if (state_index_to_ket_index[i] != last_ket &&
+                    last_ket != std::numeric_limits<int>::max()) {
                     blocks_creator.add(i);
                     break;
                 }
@@ -626,10 +631,12 @@ std::shared_ptr<const Derived> Basis<Derived>::transformed(const Sorting &transf
             state_index_to_quantum_number_m[transformation.matrix.indices()[i]];
         transformed->state_index_to_parity[i] =
             state_index_to_parity[transformation.matrix.indices()[i]];
-        transformed->state_index_to_ket_index[i] =
-            state_index_to_ket_index[transformation.matrix.indices()[i]];
-        transformed->ket_index_to_state_index
-            [state_index_to_ket_index[transformation.matrix.indices()[i]]] = i;
+
+        size_t ket_index = state_index_to_ket_index[transformation.matrix.indices()[i]];
+        transformed->state_index_to_ket_index[i] = ket_index;
+        if (ket_index != std::numeric_limits<int>::max()) {
+            transformed->ket_index_to_state_index[ket_index] = i;
+        }
     }
 
     return transformed;
