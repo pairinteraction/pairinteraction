3e6b573bbba27f982c29012fdddf35cf6be443b6
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2025-09-15 09:21:39 +0200

    fix real.py and complex.py

--- src/pairinteraction/real.py
+++ src/pairinteraction/real.py
@@ -1,18 +1,24 @@
 # SPDX-FileCopyrightText: 2025 PairInteraction Developers
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
-from pairinteraction import (
+from pairinteraction.basis import (
     BasisAtomReal as BasisAtom,
     BasisPairReal as BasisPair,
-    Database,
-    GreenTensorReal as GreenTensor,
+)
+from pairinteraction.database import Database
+from pairinteraction.diagonalize import diagonalize
+from pairinteraction.ket import (
     KetAtom,
     KetPairReal as KetPair,
+)
+from pairinteraction.state import (
     StateAtomReal as StateAtom,
     StatePairReal as StatePair,
+)
+from pairinteraction.system import (
+    GreenTensorReal as GreenTensor,
     SystemAtomReal as SystemAtom,
     SystemPairReal as SystemPair,
-    diagonalize,
 )
 
 __all__ = [
