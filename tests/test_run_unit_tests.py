a7bd7d3d19da84f908b5ac1c2027ca98cd744ed0
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2025-09-22 16:53:04 +0200

    small code improvements and fix mypy improve run_unit_tests

--- tests/test_run_unit_tests.py
+++ tests/test_run_unit_tests.py
@@ -7,4 +7,4 @@ from pairinteraction import Database, run_unit_tests
 def test_module_tests() -> None:
     """Execute the module tests."""
     database = Database.get_global_database()
-    assert run_unit_tests(database.download_missing, database_dir=database.database_dir) == 0
+    assert run_unit_tests(database.download_missing, database.use_cache, database.database_dir) == 0
