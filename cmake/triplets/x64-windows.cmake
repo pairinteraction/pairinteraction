38ca0fac779e1a9fc21af415372c2a6e38b4193e
Sebastian Weber <seweber@posteo.de>
2025-10-21 12:05:22 +0200

    Use duckdb from vcpkg under windows, let vcpkg build only release versions, update duckdb

---
+++ cmake/triplets/x64-windows.cmake
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2024 PairInteraction Developers
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
+set(VCPKG_BUILD_TYPE release)
