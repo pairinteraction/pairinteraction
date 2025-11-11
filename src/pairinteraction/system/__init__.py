9a0f0281d5cdcbf0e2d3278fc2de9d2d45d04da3
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2025-11-11 18:56:55 +0100

    git mv system/green_tensor_interpolator.py -> green_tensor/green_tensor_interpolator.py

--- src/pairinteraction/system/__init__.py
+++ src/pairinteraction/system/__init__.py
@@ -1,14 +1,11 @@
 # SPDX-FileCopyrightText: 2025 PairInteraction Developers
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
-from pairinteraction.system.green_tensor_interpolator import GreenTensorInterpolator, GreenTensorInterpolatorReal
 from pairinteraction.system.system_atom import SystemAtom, SystemAtomReal
 from pairinteraction.system.system_base import SystemBase
 from pairinteraction.system.system_pair import SystemPair, SystemPairReal
 
 __all__ = [
-    "GreenTensorInterpolator",
-    "GreenTensorInterpolatorReal",
     "SystemAtom",
     "SystemAtomReal",
     "SystemBase",
