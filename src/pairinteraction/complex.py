70ca75393a9d2ba0f3ec6162cb7e62904a38a0b4
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2025-11-11 18:57:12 +0100

    add all green tensor classes to pi namespace

--- src/pairinteraction/complex.py
+++ src/pairinteraction/complex.py
@@ -6,7 +6,12 @@ import warnings
 from pairinteraction.basis import BasisAtom, BasisPair
 from pairinteraction.database import Database
 from pairinteraction.diagonalization import diagonalize
-from pairinteraction.green_tensor import GreenTensorInterpolator
+from pairinteraction.green_tensor import (
+    GreenTensorCavity,
+    GreenTensorFreeSpace,
+    GreenTensorInterpolator,
+    GreenTensorSurface,
+)
 from pairinteraction.ket import KetAtom, KetPair
 from pairinteraction.perturbative import C3, C6, EffectiveSystemPair
 from pairinteraction.state import StateAtom, StatePair
@@ -19,7 +24,10 @@ __all__ = [
     "BasisPair",
     "Database",
     "EffectiveSystemPair",
+    "GreenTensorCavity",
+    "GreenTensorFreeSpace",
     "GreenTensorInterpolator",
+    "GreenTensorSurface",
     "KetAtom",
     "KetPair",
     "StateAtom",
