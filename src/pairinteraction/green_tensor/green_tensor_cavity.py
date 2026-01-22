fix units and z pos for surface and cavity

src/pairinteraction/green_tensor/green_tensor_cavity.py
@@ -62,23 +62,24 @@ class GreenTensorCavity(GreenTensorBase):
             raise RuntimeError("Atom positions have to be set before calculating the Green tensor.")
 
         au_to_meter = ureg.Quantity(1, "atomic_unit_of_length").to("meter").magnitude
-        pos1 = np.array(self.pos1_au) * au_to_meter
-        pos2 = np.array(self.pos2_au) * au_to_meter
+        pos1_m = np.array(self.pos1_au) * au_to_meter
+        pos2_m = np.array(self.pos2_au) * au_to_meter
         epsilon = get_electric_permitivity(self.epsilon, omega_au, "hartree")
 
-        surface1_z = self.surface1_z_au * au_to_meter
-        surface2_z = self.surface2_z_au * au_to_meter
-        omega_freq = ureg.Quantity(omega_au, "hartree").to("Hz", "spectroscopy")  # this is the angular frequency
-        omega_hz = omega_freq.magnitude
+        omega = ureg.Quantity(omega_au, "hartree").to("Hz", "spectroscopy")  # this is the angular frequency
+        omega_hz = omega.magnitude
 
-        height = abs(surface1_z - surface2_z)
-        pos1_shifted = pos1 - np.array([0, 0, min(surface1_z, surface2_z)])
-        pos2_shifted = pos2 - np.array([0, 0, min(surface1_z, surface2_z)])
+        surface1_z_m = self.surface1_z_au * au_to_meter
+        surface2_z_m = self.surface2_z_au * au_to_meter
 
-        if not 0 < pos1_shifted[2] < height or not 0 < pos2_shifted[2] < height:
+        height = abs(surface1_z_m - surface2_z_m)
+        pos1_shifted_m = pos1_m - np.array([0, 0, min(surface1_z_m, surface2_z_m)])
+        pos2_shifted_m = pos2_m - np.array([0, 0, min(surface1_z_m, surface2_z_m)])
+
+        if not 0 < pos1_shifted_m[2] < height or not 0 < pos2_shifted_m[2] < height:
             raise ValueError("Both atoms must be located inside the cavity between the two surfaces.")
 
-        if surface2_z < surface1_z:
+        if surface2_z_m < surface1_z_m:
             epsilon_top = get_electric_permitivity(self.surface1_epsilon, omega_au, "hartree")
             epsilon_bottom = get_electric_permitivity(self.surface2_epsilon, omega_au, "hartree")
         else:
@@ -86,9 +87,9 @@ class GreenTensorCavity(GreenTensorBase):
             epsilon_bottom = get_electric_permitivity(self.surface1_epsilon, omega_au, "hartree")
 
         gt = utils.green_tensor_total(
-            pos1_shifted, pos2_shifted, omega_hz, epsilon, epsilon_top, epsilon_bottom, height
+            pos1_shifted_m, pos2_shifted_m, omega_hz, epsilon, epsilon_top, epsilon_bottom, height
         )
 
         # see green_tensor_free_space for details on the prefactors
-        gt *= 4 * np.pi * (omega_freq / ureg.speed_of_light).to_base_units().m ** 2 * au_to_meter**3
+        gt *= 4 * np.pi * au_to_meter * (omega / ureg.speed_of_light).to_base_units().m ** 2
         return np.real(gt)