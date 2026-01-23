fix units again

src/pairinteraction/green_tensor/green_tensor_surface.py
@@ -54,7 +54,7 @@ class GreenTensorSurface(GreenTensorBase):
             omega_au: The frequency in atomic units at which to evaluate the Green tensor.
 
         Returns:
-            The dipole dipole Green tensor in cartesian coordinates as a 3x3 array in atomic units.
+            The dipole dipole Green tensor in cartesian coordinates as a 3x3 array in atomic units (i.e. 1/bohr).
 
         """
         if self.pos1_au is None or self.pos2_au is None:
@@ -91,8 +91,6 @@ class GreenTensorSurface(GreenTensorBase):
 
         gt = utils.green_tensor_total(
             pos1_shifted_m, pos2_shifted_m, omega_hz, epsilon, epsilon_top, epsilon_bottom, height, only_real_part=True
-        )
+        )  # 1/m
 
-        # see green_tensor_free_space for details on the prefactors
-        gt *= 4 * np.pi * au_to_meter * (omega / ureg.speed_of_light).to_base_units().m ** 2
-        return np.real(gt)
+        return np.real(gt) * au_to_meter  # 1/bohr