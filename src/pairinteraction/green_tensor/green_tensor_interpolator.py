2d49e8aee173dd36f7843939373319804f31bd54
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2026-02-02 11:46:42 +0100

    [tests] fix green tensor tests

--- src/pairinteraction/green_tensor/green_tensor_interpolator.py
+++ src/pairinteraction/green_tensor/green_tensor_interpolator.py
@@ -30,10 +30,10 @@ class GreenTensorInterpolator:
         >>> distance_mum = 5
         >>> omega = 1
         >>> tensor = np.array([[1, 0, 0], [0, 1, 0], [0, 0, -2]]) / (omega**2 * distance_mum**3)
-        >>> tensor_unit = "hartree / (e^2 micrometer^3)"
+        >>> tensor_unit = "1 / micrometer"
         >>> gt.set_constant_from_cartesian(1, 1, tensor, omega, tensor_unit, omega_unit="Hz")
         GreenTensorInterpolator(...)
-        >>> print(gt.get_spherical(1, 1, unit=tensor_unit).diagonal())
+        >>> print(gt.get_spherical(1, 1, omega, omega_unit="Hz", unit=tensor_unit).diagonal())
         [ 0.008 -0.016  0.008]
 
     """
