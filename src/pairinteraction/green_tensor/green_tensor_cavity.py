fix scaled prefactor units

src/pairinteraction/green_tensor/green_tensor_cavity.py
@@ -6,6 +6,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Callable
 
 import numpy as np
+import scipy.constants as const
 
 from pairinteraction.green_tensor import utils
 from pairinteraction.green_tensor.green_tensor_base import GreenTensorBase, get_electric_permitivity
@@ -86,8 +87,10 @@ class GreenTensorCavity(GreenTensorBase):
             epsilon_top = get_electric_permitivity(self.surface2_epsilon, omega_au, "hartree")
             epsilon_bottom = get_electric_permitivity(self.surface1_epsilon, omega_au, "hartree")
 
+        # unit: # m^(-3) [hbar]^(-1) [epsilon_0]^(-1)
         gt = utils.green_tensor_total(
             pos1_shifted_m, pos2_shifted_m, omega_hz, epsilon, epsilon_top, epsilon_bottom, height, only_real_part=True
-        )  # 1/m
-
-        return np.real(gt) * au_to_meter  # 1/bohr
+        )
+        to_au = ureg.Quantity(1, "m**3") * ((4 * np.pi) ** (-1)) / (const.epsilon_0 * const.hbar)
+        # hbar * epsilon_0 = (4*np.pi)**(-1) in atomc units
+        return np.real(gt) / to_au.to_base_units().magnitude