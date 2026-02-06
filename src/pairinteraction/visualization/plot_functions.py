4f03d0a6fef613a99431b698a7ac5b3e6edb893e
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2026-02-06 18:49:35 +0100

    update plot functions

--- src/pairinteraction/visualization/plot_functions.py
+++ src/pairinteraction/visualization/plot_functions.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -21,8 +22,20 @@ if TYPE_CHECKING:
     from pairinteraction.units import NDArray
 
 
-def plot_pair_potential(system_pairs: Sequence[SystemPair], ket_pair_of_interest: KetPairLike) -> tuple[Figure, Axes]:
-    """Plot the pair potential of a sequence of SystemPair objects."""
+def plot_pair_potential(
+    system_pairs: Sequence[SystemPair],
+    ket_pair_of_interest: KetPairLike,
+    ax: Axes | None = None,
+) -> tuple[Figure, Axes]:
+    """Plot the pair potential of a sequence of SystemPair objects.
+
+    Args:
+        system_pairs: A sequence of SystemPair objects to plot.
+        ket_pair_of_interest: The KetPair (or tuple of KetAtom) for whicht the overlaps will be plotted.
+        ax: An optional matplotlib Axes object to plot on.
+            If None, a new figure and axes will be created.
+
+    """
     distances: NDArray = np.array([system.get_distance(unit="micrometer") for system in system_pairs])
 
     if is_ket_atom_tuple(ket_pair_of_interest):
@@ -35,28 +48,36 @@ def plot_pair_potential(system_pairs: Sequence[SystemPair], ket_pair_of_interest
     eigenenergies = [system.get_eigenenergies(unit="GHz") - energy_of_interest for system in system_pairs]
     overlaps = [system.get_eigenbasis().get_overlaps(ket_pair_of_interest) for system in system_pairs]
 
-    fig, ax = plt.subplots()
+    if ax is None:
+        owns_ax = True
+        fig, ax = plt.subplots()
+    else:
+        owns_ax = False
+        fig = ax.figure
 
-    ax.set_xlabel(r"Distance [$\mu$m]")
-    ax.set_ylabel("Energy [GHz]")
-
-    ax.plot(distances, np.array(eigenenergies), c="k", lw=0.25, zorder=-10)
+    lw = mpl.rcParams["lines.linewidth"]
+    ax.plot(distances, np.array(eigenenergies), c="k", lw=lw / 6, zorder=-10)
 
     x_repeated = np.hstack([val * np.ones_like(es) for val, es in zip(distances, eigenenergies)])
     energies_flattend = np.hstack(eigenenergies)
     overlaps_flattend = np.hstack(overlaps)
     sorter = np.argsort(overlaps_flattend)
 
+    ms = mpl.rcParams["lines.markersize"]
     scat = ax.scatter(
         x_repeated[sorter],
         energies_flattend[sorter],
         c=overlaps_flattend[sorter],
-        s=15,
+        s=ms * 2.5,
         vmin=0,
         vmax=1,
         cmap=alphamagma,
     )
 
-    fig.colorbar(scat, ax=ax, label="Overlap with state of interest")
+    if owns_ax:
+        ax.set_xlabel(r"Distance ($\mu$m)")
+        ax.set_ylabel(r"Energy (h$\cdot$GHz)")
+        fig.colorbar(scat, ax=ax, label="Overlap with state of interest")
+        fig.tight_layout()
 
     return fig, ax
