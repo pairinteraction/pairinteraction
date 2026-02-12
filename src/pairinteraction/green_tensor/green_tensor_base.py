green tensor fix sign and prefactor

src/pairinteraction/green_tensor/green_tensor_base.py
@@ -152,10 +152,10 @@ class GreenTensorBase(ABC):
         """
         if kappa1 == 1 and kappa2 == 1:
             speed_of_light_au: float = ureg.Quantity(1, "speed_of_light").to_base_units().m
-            factor = (transition_energy_au / speed_of_light_au) ** 2
-            factor /= 1 / (4 * np.pi)  # hbar * epsilon_0 = 1 / (4*np.pi) in atomic units
-            factor /= (2 * np.pi) ** 2  # TODO check omega angular or normal frequency mistake somewhere?
-            return factor
+            prefactor = (transition_energy_au / speed_of_light_au) ** 2
+            prefactor /= 1 / (4 * np.pi)  # hbar = 1, epsilon_0 = 1 / (4*np.pi) in atomic units
+            prefactor *= -1  # minus sign from H = - \hbar \sum V_{\alpha\beta} ...
+            return -prefactor
         raise NotImplementedError("Only dipole-dipole Green tensor prefactor is currently implemented.")
 
     @staticmethod