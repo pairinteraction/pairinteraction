27426eb1150b7c7af29b174c61eddf8b162ff975
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2026-05-08 09:50:12 +0200

    BasisPair add get_corresponding_state for ket tuple

--- src/pairinteraction/basis/basis_pair.py
+++ src/pairinteraction/basis/basis_pair.py
@@ -252,6 +252,24 @@ class BasisPair(BasisBase[KetPair, StatePair]):
         obj.system_atoms = system_atoms
         return obj
 
+    def get_corresponding_state(self, ket: KetPairLike) -> StatePair:  # type: ignore [override]
+        state_index = self.get_corresponding_state_index(ket)
+        return self.get_state(state_index)
+
+    def get_corresponding_state_index(self, ket: KetPairLike) -> int:  # type: ignore [override]
+        if isinstance(ket, KetPair):
+            return super().get_corresponding_state_index(ket)
+        if is_ket_atom_tuple(ket):
+            overlaps = self.get_overlaps(ket)
+            id_max = np.argmax(overlaps)
+            if overlaps[id_max] < 0.51:
+                raise ValueError(
+                    "The provided ket pair does not correspond well to any state in the basis "
+                    f"(max overlap={overlaps[id_max]:.3f})."
+                )
+            return int(id_max)
+        raise TypeError(f"Unknown type: {type(ket)=}")
+
     @overload
     def get_amplitudes(self, other: KetPairLike | StatePair) -> NDArray: ...
 
