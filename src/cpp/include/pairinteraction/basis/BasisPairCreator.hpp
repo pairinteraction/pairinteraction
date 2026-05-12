f72beb7245a61dd5c2e7570138e1e23048ffaf55
Sebastian Weber <seweber@posteo.de>
2026-05-12 15:37:56 +0200

    Clean up construction of symmetrized bases

--- src/cpp/include/pairinteraction/basis/BasisPairCreator.hpp
+++ src/cpp/include/pairinteraction/basis/BasisPairCreator.hpp
@@ -3,6 +3,7 @@
 
 #pragma once
 
+#include "pairinteraction/enums/Parity.hpp"
 #include "pairinteraction/utils/Range.hpp"
 #include "pairinteraction/utils/traits.hpp"
 
@@ -33,7 +34,7 @@ public:
     using ket_t = KetPair<Scalar>;
     using ketvec_t = std::vector<std::shared_ptr<const ket_t>>;
 
-    BasisPairCreator();
+    BasisPairCreator() = default;
     BasisPairCreator<Scalar> &add(const SystemAtom<Scalar> &system_atom);
     BasisPairCreator<Scalar> &restrict_energy(real_t min, real_t max);
     BasisPairCreator<Scalar> &restrict_quantum_number_m(real_t min, real_t max);
@@ -45,8 +46,8 @@ private:
     std::vector<std::reference_wrapper<const SystemAtom<Scalar>>> systems_atom;
     Range<real_t> range_energy;
     Range<real_t> range_quantum_number_m;
-    Parity parity_under_inversion;   // NOLINT
-    Parity parity_under_permutation; // NOLINT
+    Parity parity_under_inversion{Parity::UNKNOWN};
+    Parity parity_under_permutation{Parity::UNKNOWN};
 };
 
 extern template class BasisPairCreator<double>;
