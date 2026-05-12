c830075676676888c5699ae459f74cb41b2eb4ab
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2026-05-12 11:36:33 +0200

    export C6 configs

--- src/pairinteraction_gui/config/__init__.py
+++ src/pairinteraction_gui/config/__init__.py
@@ -2,10 +2,21 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
 from pairinteraction_gui.config.base_config import BaseConfig
-from pairinteraction_gui.config.basis_config import BasisConfig, BasisConfigOneAtom, BasisConfigTwoAtoms
+from pairinteraction_gui.config.basis_config import BasisConfig, BasisConfigC6, BasisConfigOneAtom, BasisConfigTwoAtoms
 from pairinteraction_gui.config.calculation_config import CalculationConfig
-from pairinteraction_gui.config.ket_config import KetConfig, KetConfigLifetimes, KetConfigOneAtom, KetConfigTwoAtoms
-from pairinteraction_gui.config.system_config import SystemConfig, SystemConfigOneAtom, SystemConfigTwoAtoms
+from pairinteraction_gui.config.ket_config import (
+    KetConfig,
+    KetConfigC6,
+    KetConfigLifetimes,
+    KetConfigOneAtom,
+    KetConfigTwoAtoms,
+)
+from pairinteraction_gui.config.system_config import (
+    SystemConfig,
+    SystemConfigC6,
+    SystemConfigOneAtom,
+    SystemConfigTwoAtoms,
+)
 
 # The naming scheme for the config classes follows the pattern:
 # <ConfigType>Config<PageType>, e.g. the KetConfig for the OneAtomPage is called KetConfigOneAtom.
@@ -13,14 +24,17 @@ from pairinteraction_gui.config.system_config import SystemConfig, SystemConfigO
 __all__ = [
     "BaseConfig",
     "BasisConfig",
+    "BasisConfigC6",
     "BasisConfigOneAtom",
     "BasisConfigTwoAtoms",
     "CalculationConfig",
     "KetConfig",
+    "KetConfigC6",
     "KetConfigLifetimes",
     "KetConfigOneAtom",
     "KetConfigTwoAtoms",
     "SystemConfig",
+    "SystemConfigC6",
     "SystemConfigOneAtom",
     "SystemConfigTwoAtoms",
 ]
