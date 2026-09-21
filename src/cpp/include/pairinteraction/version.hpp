a6e4f570aae6e11eb2c73c5d58414d2b419446eb
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2026-09-21 10:04:35 +0200

    bump version v2.7.0

--- src/cpp/include/pairinteraction/version.hpp
+++ src/cpp/include/pairinteraction/version.hpp
@@ -6,7 +6,7 @@
 namespace pairinteraction {
 
 inline constexpr int VERSION_MAJOR = 2;
-inline constexpr int VERSION_MINOR = 6;
+inline constexpr int VERSION_MINOR = 7;
 inline constexpr int VERSION_PATCH = 0;
 
 inline constexpr int COMPATIBLE_DATABASE_VERSION_MAJOR = 2;
